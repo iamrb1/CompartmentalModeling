4872c8a4d6cb8efc66bf7c923e50c7e0b0be0fcf
Anand <22777489+pinnama7@users.noreply.github.com>
2025-01-31 14:41:35 -0500

    Create Initial Class List
    Based off of Header File and Initial Specs design

---
+++ Group-07/Team07Library/DataFileManager.cpp
@@ -0,0 +1,22 @@
+#include "DataFileManager.h"
+
+DataFileManager::DataFileManager() {
+}
+
+void DataFileManager::update() {
+}
+
+void DataFileManager::addFunction() {
+}
+
+void DataFileManager::openFile() {
+}
+
+void DataFileManager::closeFile() {
+}
+
+void DataFileManager::clearFunctions() {
+}
+
+void DataFileManager::listFunctions() {
+}
