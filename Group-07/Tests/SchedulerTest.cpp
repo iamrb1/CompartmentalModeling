7d9f1fc380730b0e4f1e4207891d169aa1898c39
nstanis21 <156991807+nstanis21@users.noreply.github.com>
2025-02-02 10:38:14 -0500

    Fix CMake so testing works

---
+++ Group-07/Tests/SchedulerTest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <Scheduler.h>
+#include "gtest/gtest.h"
+
+class SchedulerTest:public ::testing::Test
+{
+protected:
+
+};
+
+TEST_F(SchedulerTest,Construct)
+{
+
+}
