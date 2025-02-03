5176fea52bee344c311d0781cb9dd77f207de289
Anand <22777489+pinnama7@users.noreply.github.com>
2025-02-03 12:19:13 -0500

    Update Header to match changes to Constructor and openFile

--- Group-07/Team07Library/DataFileManager.h
+++ Group-07/Team07Library/DataFileManager.h
@@ -1,17 +1,19 @@
 #ifndef DATAFILEMANAGER_H
 #define DATAFILEMANAGER_H
 
+#include <string>
+
 class DataFileManager {
 public:
-    DataFileManager();
+    DataFileManager(const std::string& path); // Constructor
     void update();
     void addFunction();
     void openFile();
     void closeFile();
     void clearFunctions();
     void listFunctions();
-
 private:
+    std::string filePath;
 };
 
 #endif // DATAFILEMANAGER_H
