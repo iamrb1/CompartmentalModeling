ea8626efdbdb5e769886cc80fa42a2d6c013f67e
Sid Bhattacharya <bhatta70@msu.edu>
2025-02-03 14:40:56 -0500

    switch tests to gtest format

--- Group-07/Tests/AuditedArrayTest.cpp
+++ Group-07/Tests/AuditedArrayTest.cpp
@@ -3,53 +3,54 @@
  */
 #include <cassert>
 #include <iostream>
-
+#include "gtest/gtest.h"
 #include "AuditedArray.hpp"
 
-class AuditedArrayTest {
- public:
-  static void testAccess() {
-    AuditedArray<int, 5> arr;
-    assert(arr.size() == 5);
-
-    std::cout << "testAccess PASSED" << std::endl;
-  }
-  static void testOperators() {
-    AuditedArray<int, 5> arr;
-
-    // test []
-    arr[0] = 1;
-    assert(arr[0] == 1);
-
-    // test =
-    AuditedArray<int, 5> arr2 = arr;
-    for (int i = 0; i < 5; i++) {
-      assert(arr2[i] == arr[i]);
-    }
-    std::cout << "testOperators PASSED" << std::endl;
-  }
-  static void testHelpers() {
-    AuditedArray<int, 5> arr;
-    arr.fill(1);
-    for (int i = 0; i < 5; i++) {
-      assert(arr[i] == 1);
-    }
-    arr[1] = 3;
-    assert(arr.contains(3));
-    assert(!arr.contains(2));
-
-    assert(arr.indexOf(1) == 0);
-    assert(arr.indexOf(2) == -1);
-
-    std::cout << "testHelpers PASSED" << std::endl;
-  }
-  static void runAllTests() {
-    testAccess();
-    testOperators();
-  }
-};
-
-int main() {
-  AuditedArrayTest::runAllTests();
-  return 0;
+TEST(AuditedArrayTest, Construct) {
+  AuditedArray<int, 5> arr;
 }
+
+TEST(AuditedArrayTest, Size) {
+  AuditedArray<int, 5> arr;
+  ASSERT_EQ(arr.size(), 5);
+}
+
+TEST(AuditedArrayTest, Access) {
+  AuditedArray<int, 5> arr;
+  arr[0] = 1;
+  ASSERT_EQ(arr[0], 1);
+}
+
+TEST(AuditedArrayTest, Assignment) {
+  AuditedArray<int, 5> arr;
+  arr[0] = 1;
+  AuditedArray<int, 5> arr2 = arr;
+  for (int i = 0; i < 5; i++) {
+    ASSERT_EQ(arr2[i], arr[i]);
+  }
+}
+
+TEST(AuditedArrayTest, Fill) {
+  AuditedArray<int, 5> arr;
+  arr.fill(1);
+  for (int i = 0; i < 5; i++) {
+    ASSERT_EQ(arr[i], 1);
+  }
+}
+
+TEST(AuditedArrayTest, Contains) {
+  AuditedArray<int, 5> arr;
+  arr.fill(1);
+  arr[1] = 3;
+  ASSERT_TRUE(arr.contains(3));
+  ASSERT_FALSE(arr.contains(2));
+}
+
+TEST(AuditedArrayTest, IndexOf) {
+  AuditedArray<int, 5> arr;
+  arr.fill(1);
+  arr[1] = 3;
+  ASSERT_EQ(arr.indexOf(1), 0);
+  ASSERT_EQ(arr.indexOf(2), -1);
+}
+
