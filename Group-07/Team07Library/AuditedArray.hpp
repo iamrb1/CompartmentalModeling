4fcecedcf3b37e0403120321d0245be499a253b5
Sid Bhattacharya <bhatta70@msu.edu>
2025-02-03 09:13:31 -0500

    initial working version auditedarray

--- Group-07/Team07Library/AuditedArray.hpp
+++ Group-07/Team07Library/AuditedArray.hpp
@@ -1,62 +1,80 @@
 /*
-* @file AuditedArray.hpp
+ * @file AuditedArray.hpp
  * @author Siddhartha Bhattacharya
  */
 #ifndef AUDITEDARRAY_H
 #define AUDITEDARRAY_H
 
+#include <algorithm>
 #include <iostream>
 #include <stdexcept>
-#include <algorithm>
 
 template <typename T, size_t Size>
 class AuditedArray {
-public:
-    AuditedArray();
-    AuditedArray(const AuditedArray<T, Size>& other);
-    ~AuditedArray() = default;
-    AuditedArray<T, Size>& operator=(const AuditedArray<T, Size>& other);
-    T& operator[](int index);
-    const T& operator[](int index) const;
-    int size() const;
-    void fill(const T& value);
-    bool contains(const T& value) const;
-    size_t indexOf(const T& value) const;
-    T* begin();
-    T* end();
-    const T* begin() const;
-    const T* end() const;
+ public:
+  AuditedArray();
+  AuditedArray(const AuditedArray<T, Size>& other);
+  ~AuditedArray() = default;
+  AuditedArray<T, Size>& operator=(const AuditedArray<T, Size>& other);
+  T& operator[](int index);
+  const T& operator[](int index) const;
+  int size() const;
+  void fill(const T& value);
+  bool contains(const T& value) const;
+  size_t indexOf(const T& value) const;
+  T* begin();
+  T* end();
+  const T* begin() const;
+  const T* end() const;
 
-private:
-    T data[Size];
+ private:
+  T data[Size];
 };
-
 
 template <typename T, size_t Size>
 AuditedArray<T, Size>::AuditedArray() {
-  // TODO: Implement this function
+  // Default constructor
+  std::fill(begin(), end(), T());
 }
 
 template <typename T, size_t Size>
 AuditedArray<T, Size>::AuditedArray(const AuditedArray<T, Size>& other) {
-  // TODO: Implement this function
+  // Copy constructor
+  if (this->size() != other.size()) {
+    throw std::invalid_argument("Array sizes do not match");
+  }
+  std::copy(other.begin(), other.end(), begin());
 }
 
 template <typename T, size_t Size>
-AuditedArray<T, Size>& AuditedArray<T, Size>::operator=(const AuditedArray<T, Size>& other) {
-  // TODO: Implement this function
+AuditedArray<T, Size>& AuditedArray<T, Size>::operator=(
+    const AuditedArray<T, Size>& other) {
+  // Assignment operator
+  if (this == &other) {
+    return *this;
+  }
+  if (this->size() != other.size()) {
+    throw std::invalid_argument("Array sizes do not match");
+  }
+  std::copy(other.begin(), other.end(), begin());
+  return *this;
 }
 
 template <typename T, size_t Size>
 T& AuditedArray<T, Size>::operator[](int index) {
-  // TODO: Implement this function
+  if (index < 0 || index >= Size) {
+    throw std::out_of_range("Index out of range");
+  }
+  return data[index];
 }
 
 template <typename T, size_t Size>
 const T& AuditedArray<T, Size>::operator[](int index) const {
-  // TODO: Implement this function
+  if (index < 0 || index >= Size) {
+    throw std::out_of_range("Index out of range");
+  }
+  return data[index];
 }
-
 
 template <typename T, size_t Size>
 int AuditedArray<T, Size>::size() const {
@@ -65,37 +83,49 @@ int AuditedArray<T, Size>::size() const {
 
 template <typename T, size_t Size>
 void AuditedArray<T, Size>::fill(const T& value) {
-  // TODO: Implement this function
+  for (int i = 0; i < Size; i++) {
+    data[i] = value;
+  }
 }
 
 template <typename T, size_t Size>
 bool AuditedArray<T, Size>::contains(const T& value) const {
-  // TODO: Implement this function
+  for (int i = 0; i < Size; i++) {
+    if (data[i] == value) {
+      return true;
+    }
+  }
+  return false;
 }
 
 template <typename T, size_t Size>
 size_t AuditedArray<T, Size>::indexOf(const T& value) const {
-  // TODO: Implement this function
+  for (int i = 0; i < Size; i++) {
+    if (data[i] == value) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 template <typename T, size_t Size>
 T* AuditedArray<T, Size>::begin() {
-  // TODO: Implement this function
+  return &data[0];
 }
 
 template <typename T, size_t Size>
 T* AuditedArray<T, Size>::end() {
-  // TODO: Implement this function
+  return &data[Size];
 }
 
 template <typename T, size_t Size>
 const T* AuditedArray<T, Size>::begin() const {
-  // TODO: Implement this function
+  return &data[0];
 }
 
 template <typename T, size_t Size>
 const T* AuditedArray<T, Size>::end() const {
-  // TODO: Implement this function
+  return &data[Size];
 }
 
-#endif // AUDITEDARRAY_H
+#endif  // AUDITEDARRAY_H
--- Group-07/Tests/AuditedArrayTest.cpp
+++ Group-07/Tests/AuditedArrayTest.cpp
@@ -1,21 +1,51 @@
 /*
  * AuditedArrayTest.cpp
  */
-#include "AuditedArray.hpp"
-#include <iostream>
 #include <cassert>
+#include <iostream>
+
+#include "AuditedArray.hpp"
 
 class AuditedArrayTest {
-public:
+ public:
   static void testAccess() {
     AuditedArray<int, 5> arr;
-    assert (arr.size() == 5);
+    assert(arr.size() == 5);
 
-    std::cout<<"testAccess PASSED"<<std::endl;
+    std::cout << "testAccess PASSED" << std::endl;
   }
+  static void testOperators() {
+    AuditedArray<int, 5> arr;
 
+    // test []
+    arr[0] = 1;
+    assert(arr[0] == 1);
+
+    // test =
+    AuditedArray<int, 5> arr2 = arr;
+    for (int i = 0; i < 5; i++) {
+      assert(arr2[i] == arr[i]);
+    }
+    std::cout << "testOperators PASSED" << std::endl;
+  }
+  static void testHelpers() {
+    AuditedArray<int, 5> arr;
+    arr.fill(1);
+    for (int i = 0; i < 5; i++) {
+      assert(arr[i] == 1);
+    }
+    arr[1] = 3;
+    assert(arr.contains(3));
+    assert(!arr.contains(2));
+
+    assert(arr.indexOf(1) == 0);
+    assert(arr.indexOf(2) == -1);
+
+    std::cout << "testHelpers PASSED" << std::endl;
+  }
   static void runAllTests() {
     testAccess();
+    testOperators();
   }
 };
 
