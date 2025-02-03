dd05cb1804cc6b98a4cf497317e7f8fe16da17fd
Your Name <jiangx57@msu.edu>
2025-02-03 02:19:24 -0500

    Wrote something in the FunctionSet class, but not sure if it is right or not

--- Group-03/FunctionSet.cpp
+++ Group-03/FunctionSet.cpp
@@ -1,5 +1,75 @@
-//
-// Created by jiang on 1/29/2025.
-//
+/**
+ * @file FunctionSet.cpp
+ * @author jiang
+ */
 
 #include "FunctionSet.h"
+#include <utility>  // for std::forward
+#include <iostream> // just in case you need it
+#include <string>   // for std::string usage
+
+//
+// 1) Provide definitions for each templated member function.
+//    * You MUST precede each with: template <typename R, typename... Args>
+//    * Then: return_type FunctionSet<R, Args...>::functionName(...) { ... }
+//
+
+/**
+ * Constructor
+ */
+template <typename R, typename... Args>
+FunctionSet<R, Args...>::FunctionSet() = default;
+
+/**
+ * Destructor
+ */
+template <typename R, typename... Args>
+FunctionSet<R, Args...>::~FunctionSet() = default;
+
+/**
+ * addFunction
+ */
+template <typename R, typename... Args>
+void FunctionSet<R, Args...>::addFunction(const FunctionType& func)
+{
+    mFunctions.push_back(func);
+}
+
+/**
+ * removeFunction
+ */
+template <typename R, typename... Args>
+void FunctionSet<R, Args...>::removeFunction(const FunctionType& func)
+{
+}
+
+/**
+ * clearAll
+ */
+template <typename R, typename... Args>
+void FunctionSet<R, Args...>::clearAll()
+{
+    mFunctions.clear();
+}
+
+/**
+ * isEmpty
+ */
+template <typename R, typename... Args>
+bool FunctionSet<R, Args...>::isEmpty() const
+{
+    return mFunctions.empty();
+}
+
+/**
+ * countFunSet
+ */
+template <typename R, typename... Args>
+std::size_t FunctionSet<R, Args...>::countFunSet() const
+{
+    return mFunctions.size();
+}
+
+template class FunctionSet<int, int>;                  // For "FunctionSet<int,int>"
+template class FunctionSet<void, const std::string&>;  // For "FunctionSet<void, const std::string&>"
+// Add more lines if you use more template argument sets...
--- Group-03/FunctionSet.h
+++ Group-03/FunctionSet.h
@@ -1,16 +1,99 @@
-//
-// Created by jiang on 1/29/2025.
-//
+/**
+ * @file FunctionSet.h
+ * @author jiang
+ */
 
 #ifndef FUNCTIONSET_H
 #define FUNCTIONSET_H
 
+#include <vector>
+#include <functional>
+#include <type_traits>
+#include <stdexcept>
+#include <string> // needed if you plan to use std::string
 
+/**
+ * @brief A container that holds multiple callable objects sharing the same signature
+ *        and can invoke them all using a single call.
+ *
+ * @tparam R    The return type of the functions.
+ * @tparam Args The argument types of the functions.
+ */
+template <typename R, typename... Args>
+class FunctionSet
+{
+public:
+    using FunctionType = std::function<R(Args...)>;
 
-class FunctionSet {
+    // Constructor + Destructor
+    FunctionSet();
+    ~FunctionSet();
 
+    /**
+     * @brief Add a function to the set
+     * @param func The actual function to store
+     */
+    void addFunction(const FunctionType& func);
+
+    /**
+     * @brief Remove a function from the set (by reference).
+     *        Real usage may require a different approach, because std::function
+     *        doesn't have operator== for comparing contents.
+     */
+    void removeFunction(const FunctionType& func);
+
+    /**
+     * @brief Clear all the functions in the function set
+     */
+    void clearAll();
+
+    /**
+     * @brief Check if the set is empty
+     * @return true if empty; false otherwise
+     */
+    bool isEmpty() const;
+
+    /**
+     * @brief Return the size of the function set
+     * @return number of functions stored
+     */
+    std::size_t countFunSet() const;
+
+    /**
+     * @brief Call all functions in the set with the given arguments
+     * @param args The parameters to pass to each function
+     * @return Depending on R, returns either std::vector<R> or void
+     */
+    auto callAll(Args... args)
+    {
+        return callAllImpl(std::is_void<R>{}, args...);
+    }
+
+private:
+    std::vector<FunctionType> mFunctions;
+
+    // If R is not void, we collect results in std::vector<R>.
+    template <typename... CallArgs>
+    std::vector<R> callAllImpl(std::false_type, CallArgs&&... callArgs)
+    {
+        std::vector<R> results;
+        results.reserve(mFunctions.size());
+        for (auto &func : mFunctions)
+        {
+            results.push_back(func(std::forward<CallArgs>(callArgs)...));
+        }
+        return results;
+    }
+
+    // If R is void, we just call the functions with no return.
+    template <typename... CallArgs>
+    void callAllImpl(std::true_type, CallArgs&&... callArgs)
+    {
+        for (auto &func : mFunctions)
+        {
+            func(std::forward<CallArgs>(callArgs)...);
+        }
+    }
 };
 
-
-
-#endif //FUNCTIONSET_H
+#endif // FUNCTIONSET_H
