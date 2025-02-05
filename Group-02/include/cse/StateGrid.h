49efdee320c0f9a077c0aaaae190a88236166d9c
DominikLeisinger <152821125+DominikLeisinger@users.noreply.github.com>
2025-02-04 19:28:35 -0500

    More StateGrid functions and descriptions

--- Group-02/include/cse/StateGrid.h
+++ Group-02/include/cse/StateGrid.h
@@ -21,8 +21,16 @@ class StateGrid {
   std::vector<std::vector<std::string>> grid;
 
  public:
-  StateGrid(size_t rows, size_t cols);
+  StateGrid(size_t rows, size_t cols, std::string difficulty);
 
   void DisplayGrid();
+
+  void Setstate(int row, int col);
+
+  std::string Getstate(int row, int col);
+
+  bool Validateposition(int row, int col);
+
+  std::string Findmoves(int row, int col);
 };
 }
--- Group-02/src/cse/StateGrid.cpp
+++ Group-02/src/cse/StateGrid.cpp
@@ -12,11 +12,47 @@ namespace cse{
  * @param rows rows in grid map
  * @param cols cols in grid map
  */
-StateGrid::StateGrid(size_t rows, size_t cols) {}
+StateGrid::StateGrid(size_t rows, size_t cols, std::string difficulty) {}
 
 
 /**
  * @brief Displays the grid to user
  */
 void StateGrid::DisplayGrid() {}
+
+/**
+ * @brief Sets the specific position to occupied by agent
+ * @param row row of position
+ * @param col col of position
+ */
+void StateGrid::Setstate(int row, int col) {}
+
+/**
+ * @brief Returns the state of a grid position
+ * @param row row of position
+ * @param col col of position
+ * @return state of (row,col) position
+ */
+std::string StateGrid::Getstate(int row, int col) {
+  return std::string();
+}
+/**
+ * @brief Validates if a position can be occupied by agent
+ * @param row row of position
+ * @param col col of position
+ * @return T/F of position validity
+ */
+bool StateGrid::Validateposition(int row, int col) {
+  return false;
+}
+
+/**
+ * @brief finds all possible moves for agent from (row,col) position
+ * @param row row of position
+ * @param col col of position
+ * @return std::string of possible move directions
+ */
+std::string StateGrid::Findmoves(int row, int col) {
+  return "std::string()";
+}
 }
