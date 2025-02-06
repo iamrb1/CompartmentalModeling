25c08a1be2c4bea72b2f2cf41508c272ad47a885
LeoSSpecht <leonardo.suguimoto@stone.com.br>
2025-02-06 13:13:47 -0500

    feat: adds sample test cases

---
+++ tests/Group1/Graph/Graph.cpp
@@ -0,0 +1,10 @@
+#include "../../../third-party/Catch/single_include/catch2/catch.hpp"
+
+#include <vector>
+
+TEST_CASE("Test std::vector", "[base]")
+{
+  std::vector<int> v{0, 1, 2, 3, 4};
+
+  CHECK(v.size() == 5);
+}
