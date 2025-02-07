8ca5bcd4c95b76667ef78d14e04cc2ad250ed772
Mary Holt <merryymary@gmail.com>
2025-02-07 10:26:21 -0500

    Google styling

--- Group-09/WebLayout/WebLayout.cpp
+++ Group-09/WebLayout/WebLayout.cpp
@@ -11,36 +11,32 @@
  * Adds an image to the web layout
  * @param image to be added
  */
-void WebLayout::addImage(const Image &image)
-{
-    images.push_back(image);
+void WebLayout::addImage(const Image &image) {
+  images.push_back(image);
 }
 
 /**
  * Removes a specific image from the web layout
  * @param image to be removed
  */
-void WebLayout::removeImage(const Image &image)
-{
-    images.erase(std::remove(images.begin(), images.end(), image));
+void WebLayout::removeImage(const Image &image) {
+  images.erase(std::remove(images.begin(), images.end(), image));
 }
 
 /**
  * Adds a textbox to the web layout
  * @param textBox to be added
  */
-void WebLayout::addTextBox(const TextBox &textBox)
-{
-    textBoxes.push_back(textBox);
+void WebLayout::addTextBox(const TextBox &textBox) {
+  textBoxes.push_back(textBox);
 }
 
 /**
  * Removes a textbox from the web layout
  * @param textBox to be removed
  */
-void WebLayout::removeTextBox(const TextBox &textBox)
-{
-    textBoxes.erase(std::remove(textBoxes.begin(), textBoxes.end(), textBox));
+void WebLayout::removeTextBox(const TextBox &textBox) {
+  textBoxes.erase(std::remove(textBoxes.begin(), textBoxes.end(), textBox));
 }
 
 /**
@@ -49,20 +45,21 @@ void WebLayout::removeTextBox(const TextBox &textBox)
  * @param width width of the textbox
  * @param height height of the textbox
  */
-void WebLayout::PushTextBox(const std::string& msg, const int& width, const int& height)
-{
-    EM_ASM({
-        var msg = UTF8ToString($0);
-        var width = $1;
-        var height = $2;
-        // Finds div with presentation-zone id, where all boxes and images will be placed
-        var textBoxDiv = document.getElementById("presentation-zone");
-        if (textBoxDiv)
-        {
-            textBoxDiv.innerHTML += '<p style="border: 1px solid black; border-radius: 5px; height: ' + height + 'px; width: ' + width + 'px;">' + msg + '</p>';
-        }
-           }, msg.c_str(), width, height
-    );
+void WebLayout::PushTextBox(const std::string &msg, const int &width, const int &height) {
+  EM_ASM({
+             var msg = UTF8ToString($0);
+      var width = $1;
+      var height = $2;
+      // Finds div with presentation-zone id, where all boxes and images will be placed
+      var textBoxDiv = document.getElementById("presentation-zone");
+      if (textBoxDiv)
+      {
+        textBoxDiv.innerHTML +=
+            '<p style="border: 1px solid black; border-radius: 5px; height: ' + height + 'px; width: ' + width + 'px;">'
+                + msg + '</p>';
+      }
+         }, msg.c_str(), width, height
+  );
 }
 
 /**
@@ -73,38 +70,40 @@ void WebLayout::PushTextBox(const std::string& msg, const int& width, const int&
  * @param xLoc of image
  * @param yLoc of image
  */
-void WebLayout::PushImage(const std::string& url, const int& width, const int& height, const int& xLoc, const int& yLoc)
-{
-    EM_ASM({
-        var msg = UTF8ToString($0);
-        var width = $1;
-        var height = $2;
-        var xLoc = $3;
-        var yLoc = $4;
-        // Finds div with presentation-zone id, where all boxes and images will be placed
-        var imageBoxDiv = document.getElementById("presentation-zone");
-        if (imageBoxDiv)
-        {
-            imageBoxDiv.innerHTML += "<img src='" + msg + "' style='object-fit: fill; width:" + width + "px; height:" + height +"; left: " + xLoc + "px; top: " + yLoc + "px;' />";
-        }
-           }, url.c_str(), width, height, xLoc, yLoc
-    );
+void WebLayout::PushImage(const std::string &url,
+                          const int &width,
+                          const int &height,
+                          const int &xLoc,
+                          const int &yLoc) {
+  EM_ASM({
+             var msg = UTF8ToString($0);
+      var width = $1;
+      var height = $2;
+      var xLoc = $3;
+      var yLoc = $4;
+      // Finds div with presentation-zone id, where all boxes and images will be placed
+      var imageBoxDiv = document.getElementById("presentation-zone");
+      if (imageBoxDiv)
+      {
+        imageBoxDiv.innerHTML +=
+            "<img src='" + msg + "' style='object-fit: fill; width:" + width + "px; height:" + height + "; left: "
+                + xLoc + "px; top: " + yLoc + "px;' />";
+      }
+         }, url.c_str(), width, height, xLoc, yLoc
+  );
 }
 
 /**
  * Loads all text boxes and images on current web layout
  */
-void WebLayout::LoadPage()
-{
-    // Display text boxes
-    for(std::vector<TextBox>::iterator it = textBoxes.begin(); it != textBoxes.end(); it++)
-    {
-        PushTextBox(it->text, it->width, it->height);
-    }
+void WebLayout::LoadPage() {
+  // Display text boxes
+  for (std::vector<TextBox>::iterator it = textBoxes.begin(); it != textBoxes.end(); it++) {
+    PushTextBox(it->text, it->width, it->height);
+  }
 
-    // Display images
-    for(std::vector<Image>::iterator it = images.begin(); it != images.end(); it++)
-    {
-        PushImage(it->url, it->width, it->height, it->xLoc, it->yLoc);
-    }
+  // Display images
+  for (std::vector<Image>::iterator it = images.begin(); it != images.end(); it++) {
+    PushImage(it->url, it->width, it->height, it->xLoc, it->yLoc);
+  }
 }
--- Group-09/WebLayout/WebLayout.h
+++ Group-09/WebLayout/WebLayout.h
@@ -15,54 +15,52 @@
 /**
  * Temporary Image struct in place of Image class
  */
-struct Image{
-    int xLoc;
-    int yLoc;
-    int width;
-    int height;
-    std::string url;
-    int id;
+struct Image {
+  int xLoc;
+  int yLoc;
+  int width;
+  int height;
+  std::string url;
+  int id;
 
-    bool operator==(const Image& image) const
-    {
-        return id == image.id;
-    }
+  bool operator==(const Image &image) const {
+    return id == image.id;
+  }
 };
 
 /**
  * Temporary TextBox struct in place of TextBox class
  */
-struct TextBox{
-    int xLoc;
-    int yLoc;
-    int width;
-    int height;
-    std::string text;
-    int id;
+struct TextBox {
+  int xLoc;
+  int yLoc;
+  int width;
+  int height;
+  std::string text;
+  int id;
 
-    bool operator==(const TextBox& textBox) const
-    {
-        return id == textBox.id;
-    }
+  bool operator==(const TextBox &textBox) const {
+    return id == textBox.id;
+  }
 };
 
 class WebLayout {
-private:
-    std::vector<TextBox> textBoxes;
-    std::vector<Image> images;
-    void PushTextBox(const std::string& msg, const int& width, const int& height);
-    void PushImage(const std::string& url, const int& width, const int& height, const int& xLoc, const int& yLoc);
+ private:
+  std::vector<TextBox> textBoxes;
+  std::vector<Image> images;
+  void PushTextBox(const std::string &msg, const int &width, const int &height);
+  void PushImage(const std::string &url, const int &width, const int &height, const int &xLoc, const int &yLoc);
 
-public:
-    WebLayout() = default;
-    ~WebLayout() = default;
-    void addImage(const Image& image);
-    void removeImage(const Image& image);
-    void addTextBox(const TextBox& textBox);
-    void removeTextBox(const TextBox& textBox);
-    void LoadPage();
-    std::string GenerateJS();
-    void TransitionToPage(const WebLayout& newPage);
+ public:
+  WebLayout() = default;
+  ~WebLayout() = default;
+  void addImage(const Image &image);
+  void removeImage(const Image &image);
+  void addTextBox(const TextBox &textBox);
+  void removeTextBox(const TextBox &textBox);
+  void LoadPage();
+  std::string GenerateJS();
+  void TransitionToPage(const WebLayout &newPage);
 };
 
 #endif //CSE498_SPRING2025_GROUP_09_WEBLAYOUT_WEBLAYOUT_H
