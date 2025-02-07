a5f0725c0987ac8271169245f28ada8751541e16
Sid Bhattacharya <bhatta70@msu.edu>
2025-02-07 15:24:10 -0500

    Revert "Merge pull request #57 from CSE498/team07-manager"
    This reverts commit cb9c87c1aa999412a74996db17500a7f85ca9255, reversing
    changes made to ccb49d3b3f24284394093e25b650586f0557d58f.

--- Group-07/Team07Library/DataFileManager.cpp
+++ Group-07/Team07Library/DataFileManager.cpp
@@ -2,37 +2,24 @@
 #include <fstream> // https://stackoverflow.com/questions/13035674/how-to-read-a-file-line-by-line-or-a-whole-text-file-at-
 #include <iostream>
 
-namespace cse {
-    // Load in path to file as a string
-    DataFileManager::DataFileManager(const std::string& path) : filePath(path) {}
+// Load in path to file as a string
+DataFileManager::DataFileManager(const std::string& path) : filePath(path) {}
 
-    // Open CSV file based on location (path variable)
-    void DataFileManager::openFile() {
-        // Handles CSV Case, https://en.cppreference.com/w/cpp/string/basic_string/find_last_of
-        if (filePath.substr(filePath.find_last_of(".") + 1) == "csv") {
+// Updates the file based on provided functions and builds new rows
+void DataFileManager::update() {
+}
+
+void DataFileManager::openFile() {
+	// Handles CSV Case, https://en.cppreference.com/w/cpp/string/basic_string/find_last_of
+	if (filePath.substr(filePath.find_last_of(".") + 1) == "csv") {
         	std::cout << "File has .csv extension." << std::endl;
     	}
-        // Handles Error
-        else {
+	// Handles Error
+	else {
         	std::cout << "File does not have valid extension." << std::endl;
     	}
-    }
-
-    // Updates the file based on provided functions and builds new rows
-    void DataFileManager::update() {
-        // Iterate over the unordered_map and call each function
-        for (const auto& pair : functionMap) {
-            std::cout << "Calling " << pair.first << std::endl;
-            pair.second();  // Call the function
-        }
-    }
-
-    void addFunction(const std::string& name, const std::function<void()>& lambda){
-        auto var1 = name;
-        auto var2 = lambda;
-    }
-
-    void DataFileManager::closeFile() {
-        filePath = "";
-    }
 }
+
+void DataFileManager::closeFile() {
+}
+
