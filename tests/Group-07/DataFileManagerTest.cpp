499ef5fb32154676348550c1561766f4bf80550a
pinnama7 <pinnama7@gmail.com>
2025-02-07 15:25:31 -0500

    fixed test headers

--- tests/Group-07/DataFileManagerTest.cpp
+++ tests/Group-07/DataFileManagerTest.cpp
@@ -1,11 +1,11 @@
-#include "../Team07Library/DataFileManager.h"
-#include "gtest/gtest.h"
+#include "../../third-party/Catch/single_include/catch2/catch.hpp"
+#include "../../Group-07/Team07Library/DataFileManager.h"
 
-TEST(DataFileManagerTest, Construct) {
+TEST_CASE("DataFileManagerTest", "Construct") {
   cse::DataFileManager dfm("../CSE498-Spring2025/Group-07/Data/sample.csv");
 }
 
 // New test case to create a CSV file
-TEST(DataFileManagerTest, CreateCSVFile) {
-    ASSERT_EQ(1,1);
+TEST_CASE("DataFileManagerTest", "CreateCSVFile") {
+    REQUIRE(1 == 1);
 }
