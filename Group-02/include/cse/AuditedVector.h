b940abefc2b449d5226487a16b90488298175eb4
hawkinsma <hawkinsmt2@gmail.com>
2025-02-07 14:39:01 -0500

    Fixed spacing format and added simple tests to main for AuditedVector.

--- Group-02/include/cse/AuditedVector.h
+++ Group-02/include/cse/AuditedVector.h
@@ -6,76 +6,76 @@
 #ifndef AUDITEDVECTOR_H
 #define AUDITEDVECTOR_H
 
-#include <vector>
-#include <stdexcept>
 #include <cstddef>
+#include <stdexcept>
+#include <vector>
 
 namespace cse {
 
 #ifdef NDEBUG
-    // In release mode, just use std::vector.
-    template <typename T, typename Allocator = std::allocator<T>>
-    using vector = std::vector<T, Allocator>;
+// In release mode, just use std::vector.
+template <typename T, typename Allocator = std::allocator<T>>
+using vector = std::vector<T, Allocator>;
 
 #else
-    template <typename T, typename Allocator = std::allocator<T>>
-    class AuditedVector : public std::vector<T, Allocator> {
-    public:
-        using base_t = std::vector<T, Allocator>;
-        using base_t::base_t;
+template <typename T, typename Allocator = std::allocator<T>>
+class AuditedVector : public std::vector<T, Allocator> {
+ public:
+  using base_t = std::vector<T, Allocator>;
+  using base_t::base_t;
 
-        T& operator[](size_t index) {
-            if (index >= this->size()) {
-                throw std::out_of_range("cse::vector: index out of range");
-            }
-            return base_t::operator[](index);
-        }
+  T& operator[](size_t index) {
+    if (index >= this->size()) {
+      throw std::out_of_range("cse::vector: index out of range");
+    }
+    return base_t::operator[](index);
+  }
 
-        const T& operator[](size_t index) const {
-            if (index >= this->size()) {
-                throw std::out_of_range("cse::vector: index out of range");
-            }
-            return base_t::operator[](index);
-        }
+  const T& operator[](size_t index) const {
+    if (index >= this->size()) {
+      throw std::out_of_range("cse::vector: index out of range");
+    }
+    return base_t::operator[](index);
+  }
 
-        T& front() {
-            if (this->empty()) {
-                throw std::out_of_range("cse::vector: front() called on empty vector");
-            }
-            return base_t::front();
-        }
+  T& front() {
+    if (this->empty()) {
+      throw std::out_of_range("cse::vector: front() called on empty vector");
+    }
+    return base_t::front();
+  }
 
-        const T& front() const {
-            if (this->empty()) {
-                throw std::out_of_range("cse::vector: front() called on empty vector");
-            }
-            return base_t::front();
-        }
+  const T& front() const {
+    if (this->empty()) {
+      throw std::out_of_range("cse::vector: front() called on empty vector");
+    }
+    return base_t::front();
+  }
 
-        T& back() {
-            if (this->empty()) {
-                throw std::out_of_range("cse::vector: back() called on empty vector");
-            }
-            return base_t::back();
-        }
+  T& back() {
+    if (this->empty()) {
+      throw std::out_of_range("cse::vector: back() called on empty vector");
+    }
+    return base_t::back();
+  }
 
-        const T& back() const {
-            if (this->empty()) {
-                throw std::out_of_range("cse::vector: back() called on empty vector");
-            }
-            return base_t::back();
-        }
+  const T& back() const {
+    if (this->empty()) {
+      throw std::out_of_range("cse::vector: back() called on empty vector");
+    }
+    return base_t::back();
+  }
 
-        void pop_back() {
-            if (this->empty()) {
-                throw std::out_of_range("cse::vector: pop_back() called on empty vector");
-            }
-            base_t::pop_back();
-        }
-    };
+  void pop_back() {
+    if (this->empty()) {
+      throw std::out_of_range("cse::vector: pop_back() called on empty vector");
+    }
+    base_t::pop_back();
+  }
+};
 
 #endif
 
-}  // End cse namespace
+}  // namespace cse
 
 #endif
