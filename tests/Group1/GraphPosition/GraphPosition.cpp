3e68a49ef049aaadf98ed86abc3511e20db48ee6
ty <pritch76@msu.edu>
2025-02-09 16:09:12 -0500

    Created file for testing GraphPosition in tests folder

---
+++ tests/Group1/GraphPosition/GraphPosition.cpp
@@ -0,0 +1,8 @@
+#include "../../../third-party/Catch/single_include/catch2/catch.hpp"
+#include "../../../Group-01/GraphPosition/GraphPosition.hpp"
+
+#include <vector>
+#include <sstream>
+
+using Catch::Matchers::WithinAbs;
+
