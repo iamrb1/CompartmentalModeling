3212a9669bdcc8a8e16421aa6b3ee31297e036b9
Anand <pinnama7@msu.edu>
2025-02-10 14:24:15 -0500

    Update DataFileManagerTest.cpp
    add cpp include

--- tests/Group-07/DataFileManagerTest.cpp
+++ tests/Group-07/DataFileManagerTest.cpp
@@ -1,13 +1,10 @@
 // Anand
 #include "../../third-party/Catch/single_include/catch2/catch.hpp"
 #include "../../Group-07/Team07Library/DataFileManager.h"
+#include "../../Group-07/Team07Library/DataFileManager.cpp"
 
 TEST_CASE("DataFileManagerTest Construct", "[DataFileManager]") {
   cse::DataFileManager dfm;
-}
-
-// New test case to create a CSV file
-TEST_CASE("DataFileManagerTest MakeCSV", "[DataFileManager]") {
-    cse::DataFileManager dfm;
-    REQUIRE(1 == 1);
+  dfm.openFile("../CSE498-Spring2025-team07-manager/Group-07/Data/sample.csv");
+  REQUIRE(1 == 1);
 }
