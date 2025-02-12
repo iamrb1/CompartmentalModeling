261d254ad1c2612fc6ec5f6c9ad272bdbf3e1f0e
Your Name <jiangx57@msu.edu>
2025-02-12 13:02:38 -0500

    just push something

--- Group-03/Tests/EmptyTest.cpp
+++ Group-03/Tests/EmptyTest.cpp
@@ -5,7 +5,7 @@
  
 #include <pch.h>
 #include "gtest/gtest.h"
-
+/// This is just a default test to see if the Cmake of the test work or not.
 TEST(EmptyTest, ExampleTest){
     ASSERT_EQ(7, 7);
 }
--- Group-03/Tests/FunctionSetTest.cpp
+++ Group-03/Tests/FunctionSetTest.cpp
@@ -3,5 +3,23 @@
  * @author jiang
  */
 
+#include <pch.h>
+#include "gtest/gtest.h"
 #include <FunctionSet.h>
 
+TEST(FunctionSetTest, IsEmpty)
+{
+    FunctionSet<int, int> eX1;
+    EXPECT_TRUE(eX1.isEmpty());
+
+    FunctionSet<int, int> eX2;
+    ASSERT_EQ(eX2.countFunSet(),0U);
+}
+
+TEST(FunctionSetTest, IsAdded)
+{
+    FunctionSet<int, int> eY1;
+    eY1.addFunction([](int x){ return x + 1; });
+    eY1.addFunction([](int x){ return x * 2; });
+    ASSERT_EQ(eY1.countFunSet(),2U);
+}
