a5c3171c9ce3fa8838abbb19812b0519f0e52dbd
ryon <baldw266@msu.edu>
2025-02-12 21:31:31 -0500

    completed unit test

--- Group-08/Dev/Tests/AnnotatedWrapperTest/AnnotatedWrapperTest.cpp
+++ Group-08/Dev/Tests/AnnotatedWrapperTest/AnnotatedWrapperTest.cpp
@@ -4,12 +4,13 @@
 TEST_CASE("Test cse::AnnotatedWrapper", "[base]")
 {
     AnnotatedWrapper wrapper;
-    wrapper.addAnnotation("key1", "value1");
-    wrapper.addAnnotation("key2", "value2");
-    wrapper.addAnnotation("key3", "value3");
-
+    
     SECTION("Check if annotations are added correctly")
     {
+        wrapper.addAnnotation("key1", "value1");
+        wrapper.addAnnotation("key2", "value2");
+        wrapper.addAnnotation("key3", "value3");
+        
         REQUIRE(wrapper.getAnnotation("key1") == "value1");
         REQUIRE(wrapper.getAnnotation("key2") == "value2");
         REQUIRE(wrapper.getAnnotation("key3") == "value3");
@@ -19,6 +20,14 @@ TEST_CASE("Test cse::AnnotatedWrapper", "[base]")
     {
         wrapper.removeAnnotation("key2");
         REQUIRE(wrapper.getAnnotation("key2") == "");
+    }
+
+    SECTION("Check if annotations are cleared correctly")
+    {
+        wrapper.clearAnnotations();
+        REQUIRE(wrapper.getAnnotation("key1") == "");
+        REQUIRE(wrapper.getAnnotation("key2") == "");
+        REQUIRE(wrapper.getAnnotation("key3") == "");
     }
 
 }
