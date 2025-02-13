c6fdb713d4a3c5801e6baf97c6b98219fec917ae
KrishPatel198 <krp198123@gmail.com>
2025-02-12 19:59:06 -0500

    wrapped class in debug guard

--- Group-04/lib/CseString.hpp
+++ Group-04/lib/CseString.hpp
@@ -4,8 +4,12 @@
 #include <string>
 
 namespace cse {
+
+#ifndef NDEBUG
+
 class String : public std::string {
  public:
+  // Inherit std::string's constructors
   String() : std::string() {}
   String(const std::string &other) : std::string(other) {}
 
@@ -15,21 +19,27 @@ class String : public std::string {
   }
 
   char &operator[](std::size_t pos) {
-#ifndef NDEBUG
+    // Range check only in debug mode
     if (pos >= size()) {
       throw std::out_of_range("cse::String index out of range");
     }
-#endif
     return std::string::operator[](pos);
   }
 
   const char &operator[](std::size_t pos) const {
-#ifndef NDEBUG
+    // Range check only in debug mode
     if (pos >= size()) {
       throw std::out_of_range("cse::String index out of range");
     }
-#endif
     return std::string::operator[](pos);
   }
 };
+
+#else
+
+// In Release builds, String is just an alias to std::string
+using String = std::string;
+
+#endif // NDEBUG
+
 }  // namespace cse
