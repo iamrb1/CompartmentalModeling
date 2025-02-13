fcd9f0dca4c9d8dbb2f973459ab3c1e114a37478
Anand <pinnama7@msu.edu>
2025-02-12 20:35:00 -0500

    Update DataFileManagerTest.cpp

--- tests/Group-07/DataFileManagerTest.cpp
+++ tests/Group-07/DataFileManagerTest.cpp
@@ -6,5 +6,28 @@
 TEST_CASE("DataFileManagerTest Construct", "[DataFileManager]") {
   cse::DataFileManager dfm;
   dfm.openFile("../CSE498-Spring2025-team07-manager/Group-07/Data/sample.csv");
+
+  // Based on Example given by prof
+  int x = 5;
+  int y = 10;
+
+  dfm.addFunction( "X Value", [&x](){return x;} );
+  dfm.addFunction( "Y Value", [&y](){return y;} );
+  dfm.listFunctions();
+
+  dfm.update();
+  x++;
+  dfm.update();
+  y++;
+  dfm.update();
+
+  // Test to make sure update doesn't work
+  dfm.deleteFunction("X Value");
+  dfm.update();
+
+  // Test clear and closeFile
+  dfm.clearFunctions();
+  dfm.closeFile();
+
   REQUIRE(1 == 1);
 }
