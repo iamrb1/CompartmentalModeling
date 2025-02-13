1eeea876185894a3d18ab1193e9157f4c3d5c05f
tdkduong <tdkduong@gmail.com>
2025-02-13 14:51:42 -0500

    Modify code structures, implement Serializer so that it works for std::string

--- Group-07/Team07Library/Serializer.h
+++ Group-07/Team07Library/Serializer.h
@@ -22,48 +22,67 @@ namespace cse
     {
     public:
         // Constructor
-        Serializer(Mode mode);
+        explicit Serializer(Mode mode) : mode_(mode) {}
+
         // Generic Serialize and Deserialze method for simple types, based on current mode
         template <typename T>
-        void Serialize(T &data, const std::string &filename);
+        void Serialize(T &data, const std::string &filename)
+        {
+            if (mode_ == Mode::SAVE)
+            {
+                std::ofstream outFile(filename, std::ios::binary);
+                if (!outFile)
+                {
+                    std::cerr << "Error opening file for writing: " << filename << std::endl;
+                    return;
+                }
+                outFile.write(reinterpret_cast<const char *>(&data), sizeof(T));
+            }
+            else
+            {
+                std::ifstream inFile(filename, std::ios::binary);
+                if (!inFile)
+                {
+                    std::cerr << "Error opening file for reading: " << filename << std::endl;
+                    return;
+                }
+                inFile.read(reinterpret_cast<char *>(&data), sizeof(T));
+            }
+        }
+
+        // Serialize std::string
+        void Serialize(std::string &data, const std::string &filename)
+        {
+            if (mode_ == Mode::SAVE)
+            {
+                std::ofstream outFile(filename, std::ios::binary);
+                if (!outFile)
+                {
+                    std::cerr << "Error opening file for writing: " << filename << std::endl;
+                    return;
+                }
+                size_t length = data.size();
+                outFile.write(reinterpret_cast<const char *>(&length), sizeof(size_t));
+                outFile.write(data.c_str(), length);
+            }
+            else
+            {
+                std::ifstream inFile(filename, std::ios::binary);
+                if (!inFile)
+                {
+                    std::cerr << "Error opening file for reading: " << filename << std::endl;
+                    return;
+                }
+                size_t length;
+                inFile.read(reinterpret_cast<char *>(&length), sizeof(size_t));
+                data.resize(length);
+                inFile.read(&data[0], length);
+            }
+        }
 
     private:
         Mode mode_;
     };
 } // namespace cse
-
-namespace cse
-{
-    Serializer::Serializer(Mode mode)
-        : mode_(mode) {}
-    template <typename T>
-    void Serializer::Serialize(T &data, const std::string &filename)
-    {
-        if (mode_ == Mode::SAVE)
-        {
-            std::ofstream OutFile(filename, std::ios::binary);
-            if (!OutFile)
-            {
-                std::cerr << "Error opening file for writing: " << filename << std::endl;
-                return;
-            }
-            OutFile.write(reinterpret_cast<const char *>(&data), sizeof(T));
-            OutFile.close();
-        }
-        else if (mode_ == Mode::LOAD)
-        {
-            {
-                std::ifstream InFile(filename, std::ios::binary);
-                if (!InFile)
-                {
-                    std::cerr << "Error opening file for reading: " << filename << std::endl;
-                    return;
-                }
-                InFile.read(reinterpret_cast<char *>(&data), sizeof(T));
-                InFile.close();
-            }
-        }
-    }
-}
 
 #endif // CSE_SERIALIZER_H
--- tests/Group-07/SerializerTest.cpp
+++ tests/Group-07/SerializerTest.cpp
@@ -2,7 +2,13 @@
 #include "../../Group-07/Team07Library/Serializer.h"
 
 #include <iostream>
+#include <random>
 #include <fstream>
+
+#include <climits>
+
+std::random_device rd;
+std::mt19937_64 gen(rd());
 
 TEST_CASE("Serializer Construction", "[Serializer]")
 {
@@ -10,50 +16,97 @@ TEST_CASE("Serializer Construction", "[Serializer]")
     cse::Serializer Loader(cse::Mode::LOAD);
 }
 
-TEST_CASE("Serializer Save", "[Serializer]")
-{
-    cse::Serializer Saver(cse::Mode::SAVE);
-    struct Person
-    {
-        char name[100];
-        int age;
-        double height;
-    };
-    Person P1 = {"tdkduong", 22, 167.5};
-    Saver.Serialize(P1, "TestP1.bin");
-    std::ifstream inFile("TestP1.bin", std::ios::binary);
-    REQUIRE(inFile);
-    std::streamsize TrueSize = sizeof(Person);
-    std::ifstream file("TestP1.bin", std::ios::binary | std::ios::ate);
-    std::streamsize TestSize = file.tellg();
-    REQUIRE(TrueSize == TestSize);
-    Person P2;
-    inFile.read(reinterpret_cast<char *>(&P2), sizeof(Person));
-    inFile.close();
-    for (int i = 0; i < 100; i++)
-        REQUIRE(P1.name[i] == P2.name[i]);
-    REQUIRE(P1.age == P2.age);
-    REQUIRE(P1.height == P2.height);
-    std::remove("TestP1.bin");
-}
-
-TEST_CASE("Serializer Load", "Serializer")
+TEST_CASE("Serializer Save Load Basic", "[Serializer]")
 {
     cse::Serializer Saver(cse::Mode::SAVE);
     cse::Serializer Loader(cse::Mode::LOAD);
-    struct Person
-    {
-        char name[100];
-        int age;
-        double height;
-    };
-    Person P1 = {"tdkduong", 22, 167.5};
-    Saver.Serialize(P1, "TestP1.bin");
-    Person P2;
-    Loader.Serialize(P2, "TestP1.bin");
+    const char filename[] = "number.bin";
+    int number = 22;
+    int result = -1;
+    Saver.Serialize(number, filename);
+    REQUIRE(result == -1);
+    Loader.Serialize(result, filename);
+    REQUIRE(result == number);
+    std::remove(filename);
+}
+
+TEST_CASE("Serializer Save Load Fundamental Types", "[Serializer]")
+{
+    cse::Serializer Saver(cse::Mode::SAVE);
+    cse::Serializer Loader(cse::Mode::LOAD);
+    const char filename[] = "result.bin";
+    std::remove(filename);
+    // Testing Int
+    std::uniform_int_distribution<int> ranInt(INT_MIN, INT_MAX);
+    int Integer;
     for (int i = 0; i < 100; i++)
-        REQUIRE(P1.name[i] == P2.name[i]);
-    REQUIRE(P1.age == P2.age);
-    REQUIRE(P1.height == P2.height);
-    std::remove("TestP1.bin");
+    {
+        Integer = ranInt(gen);
+        int Result;
+        Saver.Serialize(Integer, filename);
+        Loader.Serialize(Result, filename);
+        REQUIRE(Integer == Result);
+        std::remove(filename);
+    }
+    // Testing Char
+    std::uniform_int_distribution<char> ranChar('A', 'Z');
+    char Character;
+    for (int i = 0; i < 100; i++)
+    {
+        Character = ranChar(gen);
+        char Result;
+        Saver.Serialize(Character, filename);
+        Loader.Serialize(Result, filename);
+        REQUIRE(Character == Result);
+        std::remove(filename);
+    }
+    // Testing Long Long
+    std::uniform_int_distribution<long long> ranLL(LLONG_MIN, LLONG_MAX);
+    long long LL;
+    for (int i = 0; i < 100; i++)
+    {
+        LL = ranLL(gen);
+        long long Result;
+        Saver.Serialize(LL, filename);
+        Loader.Serialize(Result, filename);
+        REQUIRE(LL == Result);
+        std::remove(filename);
+    }
+    // Testing Double
+    std::uniform_real_distribution<double> ranDBL(-1000.0, 1000.0);
+    double Double;
+    for (int i = 0; i < 100; i++)
+    {
+        Double = ranDBL(gen);
+        double Result;
+        Saver.Serialize(Double, filename);
+        Loader.Serialize(Result, filename);
+        REQUIRE(Double == Result);
+        std::remove(filename);
+    }
+}
+
+TEST_CASE("Serializer Save Load String", "[Serializer]")
+{
+    cse::Serializer Saver(cse::Mode::SAVE);
+    cse::Serializer Loader(cse::Mode::LOAD);
+    const char filename[] = "result.bin";
+    std::remove(filename);
+    for (int i = 0; i < 100; i++)
+    {
+        std::uniform_int_distribution<int> ranInt(1, 255);
+        std::uniform_int_distribution<char> ranChar(' ', '~');
+        int Length = ranInt(gen);
+        std::string String = "";
+        for (int l = 0; l < Length; l++)
+        {
+            char Character = ranChar(gen);
+            String = String + Character;
+        }
+        std::string Result;
+        Saver.Serialize(String, filename);
+        Loader.Serialize(Result, filename);
+        REQUIRE(String == Result);
+        std::remove(filename);
+    }
 }
