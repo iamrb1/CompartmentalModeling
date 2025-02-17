aba50853467b35256d7a63375bda6923f8e37781
Mary Holt <merryymary@gmail.com>
2025-02-16 23:58:57 -0500

    Added Positioning of Textboxes + Images

--- Group-09/WebLayout/main.cpp
+++ Group-09/WebLayout/main.cpp
@@ -16,27 +16,29 @@ int main() {
   // Setup test Image
   Image *testImage = new Image(
       "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Michigan_State_Athletics_logo.svg/640px-Michigan_State_Athletics_logo.svg.png",
-      250,
-      250);
-  sampleWebLayout->addImage(*testImage);
+      25,
+      25);
+  ImageLayout il(*testImage, 20, 30);
+  sampleWebLayout->addImage(il);
 
   // Setup test textbox
   TextBoxConfig tbc;
-  tbc.height = 50;
-  tbc.width = 400;
+  tbc.height = 10;
+  tbc.width = 45;
   tbc.text = "Yay a Text Box!";
   TextBox *testTextBox = new TextBox(tbc);
-  sampleWebLayout->addTextBox(*testTextBox);
+  TextBoxLayout tbl(*testTextBox, 40, 30);
+  sampleWebLayout->addTextBox(tbl);
 
-  // Setup test textbox2
+  // Setup test textbox2 (exceed barriers test)
   TextBoxConfig tbc2;
-  tbc.height = 150;
-  tbc.width = 200;
-  tbc.text = "Yay another Text Box!!!";
-  TextBox *testTextBox2 = new TextBox(tbc);
-  sampleWebLayout->addTextBox(*testTextBox2);
+  tbc2.height = 15;
+  tbc2.width = 20;
+  tbc2.text = "Yay another Text Box!!!";
+  TextBox *testTextBox2 = new TextBox(tbc2);
+  TextBoxLayout tbl2(*testTextBox2, 500, 500);
+  sampleWebLayout->addTextBox(tbl2);
 
   // Load Page
   sampleWebLayout->LoadPage();
-  WebLayout *sampleWebLayout2 = new WebLayout();
 }
