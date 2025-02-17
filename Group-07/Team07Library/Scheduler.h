b4c3a7f99158c5f5c2c251e50c925d771b9b2814
nstanis21 <156991807+nstanis21@users.noreply.github.com>
2025-02-16 19:45:33 -0500

    Added stable sorting method and documentation

--- Group-07/Team07Library/Scheduler.h
+++ Group-07/Team07Library/Scheduler.h
@@ -14,21 +14,109 @@
 
 namespace cse
 {
+    /*
+     * Priority queue to schedule processes to run based on how often each process is used
+     */
     class Scheduler
     {
     private:
-
+        /// Maps each process ID to the number of times that the process is currently in the scheduler
         std::map<int, int> scheduleMap;
-        std::map<int, double> timesCalled;
+
+        /// Maps each process ID to the number of times that process has been added to the scheduler
+        std::map<int, int> timesCalled;
+
+        /// List holding the IDs of all the items added to the scheduler
         std::vector<int> currIds;
+
+        /// The current number of processes that are in the scheduler
         int currProcesses = 0;
-        double totalProcesses = 0;
+
+        /// The total number of processes that have been called to the scheduler
+        int totalProcesses = 0;
+
+        /// Indicator if we need to sort the scheduler again
         bool needUpdate = true;
 
+        //Code for merge and merge sort taken from https://www.geeksforgeeks.org/merge-sort/
+        void merge(std::vector<int>& arr, int left,
+                   int mid, int right)
+        {
+            int n1 = mid - left + 1;
+            int n2 = right - mid;
+
+            // Create temp vectors
+            std::vector<int> L(n1), R(n2);
+
+            // Copy data to temp vectors L[] and R[]
+            for (int i = 0; i < n1; i++)
+                L[i] = arr[left + i];
+            for (int j = 0; j < n2; j++)
+                R[j] = arr[mid + 1 + j];
+
+            int i = 0, j = 0;
+            int k = left;
+
+            // Merge the temp vectors back
+            // into arr[left..right]
+            while (i < n1 && j < n2) {
+                //Determine scheduling using all time frequency of the process ID being added to the schedule, higher frequency = higher priority
+                if (timesCalled[L[i]] >= timesCalled[R[j]]) {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            // Copy the remaining elements of L[],
+            // if there are any
+            while (i < n1) {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            // Copy the remaining elements of R[],
+            // if there are any
+            while (j < n2) {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
+        // begin is for left index and end is right index
+        // of the sub-array of arr to be sorted
+        void mergeSort(std::vector<int>& arr, int left, int right)
+        {
+            if (left >= right)
+                return;
+
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+
     public:
+        /**
+         * Default Constructor
+         */
         Scheduler() = default;
+
+        /**
+         * Default Destructor
+         */
         ~Scheduler() = default;
 
+        /**
+         * Adds a process to the Scheduler
+         * @param id ID of the process we are adding to the scheduler
+         */
         void AddProcess(int id)
         {
             scheduleMap[id]++;
@@ -41,6 +129,11 @@ namespace cse
             currProcesses++;
             needUpdate=true;
         }
+
+        /**
+         * Removes the process of a given ID from the scheduler
+         * @param id ID of the process we are removing from the scheduler
+         */
         void RemoveProcess(int id)
         {
             if(scheduleMap.find(id)!=scheduleMap.end() && scheduleMap[id]>0)
@@ -56,6 +149,12 @@ namespace cse
             }
             needUpdate=true;
         }
+
+        /**
+         * Updates the number of times a process ID occurs in the scheduler
+         * @param id ID of the process we are updating the process count for
+         * @param x The new process count
+         */
         void UpdateProcessCount(int id, int x)
         {
             if(x<0){return;}
@@ -80,16 +179,17 @@ namespace cse
             currProcesses+=x;
             needUpdate=true;
         }
+
+        /**
+         * Gets the ID for the highest priority process according to the scheduler, and removes it from the scheduler
+         * @return ID of the highest priority process
+         */
         int GetNextProcess()
         {
             if(currProcesses==0){return -1;}
             if(needUpdate)
             {
-                std::sort(currIds.begin(), currIds.end(), [this](const int &a, const int &b)
-                {
-                    return (timesCalled[a] / totalProcesses) > (timesCalled[b] / totalProcesses);
-                    //Determine scheduling using all time frequency of the process ID being added to the schedule, higher frequency = higher priority
-                });
+                mergeSort(currIds,0,currIds.size()-1);
             }
             int outID=currIds[0];
             scheduleMap[outID]--;
@@ -101,9 +201,29 @@ namespace cse
             needUpdate=false;
             return outID;
         }
+
+        /**
+         * Check if the scheduler is empty
+         * @return True if scheduler is empty, false otherwise
+         */
         bool empty() { return currProcesses == 0; }
+
+        /**
+         * Get the number of unique processes in the scheduler
+         * @return Number of unique processes in the scheduler
+         */
         int GetUniqueProcesses() const { return currIds.size(); }
+
+        /**
+         * Get the number of processes currently in the scheduler
+         * @return Number of processes currently in the scheduler
+         */
         int GetCurrProcesses() const { return currProcesses; }
+
+        /**
+         * Get the total number of processes that have been in the scheduler
+         * @return Total number of processes that have been in the scheduler
+         */
         int GetTotalProcesses() const { return totalProcesses; }
 
     };
