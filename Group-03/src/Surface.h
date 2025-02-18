666c2bffe78bda192f6c28200fa657c23a6cb8d9
vernonr2 <vernonr2@msu.edu>
2025-02-17 23:37:15 -0500

    Fixed Running Errors, tests should be runnable now using make

---
+++ Group-03/src/Surface.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+#include "Sector.h"
+
+namespace cse{
+class Surface {
+    public:
+        int width, height;
+        int sector_width, sector_height, surface_size;
+        std::vector<std::vector<Sector>> sectors;
+        
+        Surface(int width, int height, int sector_size);
+
+        void add_circle(Circle* circle);
+        void move_circle(Circle* circle, float x_pos, float y_pos);
+        void check_collision();
+        // void update();
+};
+}
