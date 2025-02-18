ea70db95b7f5e35da71a6b354338ca149c2c49eb
Anand <pinnama7@msu.edu>
2025-02-17 20:02:53 -0500

    Docstrings in DataFileManager.h

--- Group-07/Team07Library/DataFileManager.h
+++ Group-07/Team07Library/DataFileManager.h
@@ -10,68 +10,106 @@
 #include <vector>
 
 namespace cse {
-    // Class to manage data file operations, particularly CSV files
+
+    /**
+     * @class DataFileManager
+     * @brief Manages data file operations, particularly CSV files.
+     */
     class DataFileManager {
     private:
         // https://stackoverflow.com/questions/55520876/creating-an-unordered-map-of-stdfunctions-with-any-arguments
-        std::unordered_map<std::string, std::function<int()>> functionMap;  // Stores functions for data generation
-        std::string fileLocation;  // Path to the CSV file
-        std::vector<std::vector<std::string>> fileData;  // Data read from the CSV file
-        bool updateMade;  // Flag indicating if an update has been made to the data
-
+        std::unordered_map<std::string, std::function<int()>> functionMap;  ///< Stores functions for data generation.
+        std::string fileLocation;  ///< Path to the CSV file.
+        std::vector<std::vector<std::string>> fileData;  ///< Data read from the CSV file.
+        bool updateMade;  ///< Flag indicating if an update has been made to the data.
+    
     public:
-        // Default constructor
+        /**
+         * @brief Default constructor.
+         */
         DataFileManager() = default;
-
-        // Parameterized constructor initializes file location and opens the file
+    
+        /**
+         * @brief Parameterized constructor initializes file location and opens the file.
+         * @param path The file path of the CSV to open.
+         */
         DataFileManager(const std::string& path) : fileLocation(path), updateMade(false) {
             openFile(path);
         }
-
-        // Destructor
+    
+        /**
+         * @brief Destructor.
+         */
         ~DataFileManager() = default;
-
-        // Returns the file location
+    
+        /**
+         * @brief Returns the file location.
+         * @return The file location as a string.
+         */
         const std::string& getFile() const {
             return fileLocation;
         }
-
-        // Set the file location
+    
+        /**
+         * @brief Sets the file location.
+         * @param path The new file location.
+         */
         void setFile(const std::string& path) {
             fileLocation = path;
         }
-
-        // Check if an update has been made
+    
+        /**
+         * @brief Checks if an update has been made.
+         * @return True if an update has been made, otherwise false.
+         */
         bool checkUpdate() const {
             return updateMade;
         }
-
-        // Set the update flag
+    
+        /**
+         * @brief Sets the update flag.
+         * @param status The new update status.
+         */
         void setUpdate(bool status) {
             updateMade = status;
         }
-
-        // Set the data read from our file
+    
+        /**
+         * @brief Sets the data read from the file.
+         * @param data The data read from the file.
+         */
         void setData(const std::vector<std::vector<std::string>>& data) {
             fileData = data;
         }
-
-        // Return data read from the current file
+    
+        /**
+         * @brief Returns data read from the current file.
+         * @return The data read from the current file.
+         */
         std::vector<std::vector<std::string>> getData() const {
             return fileData;
         }
-
-        // Clears all stored functions
+    
+        /**
+         * @brief Clears all stored functions.
+         */
         void clearFunctions() {
             functionMap.clear();
         }
-
-        // Adds a function to the map
+    
+        /**
+         * @brief Adds a function to the map.
+         * @param name The name of the function.
+         * @param lambda The function to add.
+         */
         void addFunction(const std::string& name, const std::function<int()>& lambda) {
             functionMap[name] = lambda;
         }
-
-        // Deletes a function from our map
+    
+        /**
+         * @brief Deletes a function from the map.
+         * @param name The name of the function to delete.
+         */
         void deleteFunction(const std::string& name) {
             // https://cplusplus.com/reference/unordered_map/unordered_map/erase/
             auto it = functionMap.find(name);
@@ -79,24 +117,34 @@ namespace cse {
                 functionMap.erase(it);
             }
         }
-
-        // Lists all store functions
+    
+        /**
+         * @brief Lists all stored functions.
+         */
         void listFunctions() const {
             // https://stackoverflow.com/questions/22880431/iterate-through-unordered-map-c
             for (const auto& [key, value] : functionMap) {
                 std::cout << key << std::endl;
             }
         }
-
-        // Opens a CSV file to read its contents
+    
+        /**
+         * @brief Opens a CSV file to read its contents.
+         * @param path The file path of the CSV to open.
+         */
         void openFile(const std::string& path);
-
-        // Updates our CSV file with new data generated from functions
+    
+        /**
+         * @brief Updates the CSV file with new data generated from functions.
+         */
         void update();
-
-        // Closes the file and writes any updates made to the data
+    
+        /**
+         * @brief Closes the file and writes any updates made to the data.
+         */
         void closeFile();
     };
+
 }
 
 #endif
