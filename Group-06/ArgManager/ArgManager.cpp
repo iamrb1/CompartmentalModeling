04d1986c5924d0a58db0ce866fb3d11a6fe7eea6
Efe Caylan <103333941+matrix373@users.noreply.github.com>
2025-02-17 23:24:18 -0500

    ArgManager class clang formatted

--- Group-06/ArgManager/ArgManager.cpp
+++ Group-06/ArgManager/ArgManager.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <vector>
+
 #include "ArgManager.h"
 
 /**
@@ -13,40 +14,35 @@
  * @param argc the amount of arguments
  * @param argv the list of arguments
  */
-cse::ArgManager::ArgManager(int argc, char *argv[])
-{
-    //Load in all the command line options
-    LoadArgManager();
+cse::ArgManager::ArgManager(int argc, char* argv[]) {
+  // Load in all the command line options
+  LoadArgManager();
 
-    //pass argc and argv
-    mArgc = argc;
-    for (int i = 0; i < argc; i++) {
-        mArgv.push_back(argv[i]);
-    }
+  // pass argc and argv
+  mArgc = argc;
+  for (int i = 0; i < argc; i++) {
+    mArgv.push_back(argv[i]);
+  }
 }
 
 /**
  * @brief Loads in all of the command line options
  */
-void cse::ArgManager::LoadArgManager()
-{
-    
-}
+void cse::ArgManager::LoadArgManager() {}
 
 /**
  * @brief Checks if a string is present in the input arguments
  * @param argv the string to check for
  * @return if the string is present in the input arguments
  */
-bool cse::ArgManager::Has(std::string argv)
-{
-    // Search through each given arg in argv to check if it has the arg value
-    for (int i = 0 ; i < mArgc; i++) {
-        if (mArgv[i] == argv) {
-            return true;
-        }
+bool cse::ArgManager::Has(std::string argv) {
+  // Search through each given arg in argv to check if it has the arg value
+  for (int i = 0; i < mArgc; i++) {
+    if (mArgv[i] == argv) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 /**
@@ -54,97 +50,97 @@ bool cse::ArgManager::Has(std::string argv)
  * @param argv the string to find
  * @return the next string as the value of the option
  */
-std::string cse::ArgManager::GetOption(std::string argv)
-{
-    // Search through each given arg in argv
-    for (int i = 0 ; i < mArgc; i++) {
-        if (mArgv[i] == argv) {
-            //Check if the arg given is the last argument in the argv list, with no valid string after
-            if (i + 1 >= mArgc) {
-                std::cout << "Invalid output provided for: " << argv << std::endl;
-                return "";
-            }
-            else {
-                return mArgv[i + 1];
-            }
-        }
+std::string cse::ArgManager::GetOption(std::string argv) {
+  // Search through each given arg in argv
+  for (int i = 0; i < mArgc; i++) {
+    if (mArgv[i] == argv) {
+      // Check if the arg given is the last argument in the argv list, with no
+      // valid string after
+      if (i + 1 >= mArgc) {
+        std::cout << "Invalid output provided for: " << argv << std::endl;
+        return "";
+      } else {
+        return mArgv[i + 1];
+      }
     }
-    std::cout << "This arg does not exist: " << argv << std::endl;
-    // Return a blank string if no arg has been found
-    return "";
+  }
+  std::cout << "This arg does not exist: " << argv << std::endl;
+  // Return a blank string if no arg has been found
+  return "";
 }
 
 /**
- * @brief Find an option, then get the number after it to determine the size of the list, then return the next values in a list as the value of the option
+ * @brief Find an option, then get the number after it to determine the size of
+ * the list, then return the next values in a list as the value of the option
  * @param argv the string to find
  * @return the next strings in a list as the value of the option
  */
-std::vector<std::string> cse::ArgManager::GetOptions(std::string argv)
-{
-    int count = -1;
-    int startingIndex = -1;
-    std::vector<std::string> toReturn = {};
+std::vector<std::string> cse::ArgManager::GetOptions(std::string argv) {
+  int count = -1;
+  int startingIndex = -1;
+  std::vector<std::string> toReturn = {};
 
-    // Search through each given arg in argv
-    for (int i = 0 ; i < mArgc; i++) {
-        if (mArgv[i] == argv) {
-            // Check if the argv given is the last argument in the arg list, with no valid string after
-            if (i + 1 >= mArgc) {
-                std::cout << "Invalid output provided for: " << argv << std::endl;
-                return {""};
-            }
-            else if (count == -1) {
-                // Check the first value after the arg given, which should be the size of the list
-                
-                // Remember the starting position of the list
-                startingIndex = i + 1;
-
-                // Try and convert the value into an into
-                try {
-                    count = std::stoi(mArgv[i + 1]);
-                }
-                catch(const std::exception& caughtError) {
-                    std::cerr << "\"" << mArgv[i + 1] << "\" is not an integer" << std::endl;
-
-                    // Return a list with an empty string
-                    return {""};
-                }
-
-                // There are not enough arg given after the size provided
-                if (startingIndex + count >= mArgc)
-                {
-                    // Return a list with an empty string
-                    std::cout << "Not enough options provided for: " << argv << std::endl;
-                    return {""};
-                }
-            }
-        }
-        else if (i > startingIndex && i <= startingIndex + count) {
-            // Add the string to the list
-            toReturn.push_back(mArgv[i]);
-        }
-    }
-
-    // If the arg does not exist
-    if (count == -1) {
-        std::cout << "This arg does not exist: " << argv << std::endl;
-
-        // Return an empty string
+  // Search through each given arg in argv
+  for (int i = 0; i < mArgc; i++) {
+    if (mArgv[i] == argv) {
+      // Check if the argv given is the last argument in the arg list, with no
+      // valid string after
+      if (i + 1 >= mArgc) {
+        std::cout << "Invalid output provided for: " << argv << std::endl;
         return {""};
-    }
+      } else if (count == -1) {
+        // Check the first value after the arg given, which should be the size
+        // of the list
 
-    // Return the list created
-    return toReturn;
+        // Remember the starting position of the list
+        startingIndex = i + 1;
+
+        // Try and convert the value into an into
+        try {
+          count = std::stoi(mArgv[i + 1]);
+        } catch (const std::exception& caughtError) {
+          std::cerr << "\"" << mArgv[i + 1] << "\" is not an integer"
+                    << std::endl;
+
+          // Return a list with an empty string
+          return {""};
+        }
+
+        // There are not enough arg given after the size provided
+        if (startingIndex + count >= mArgc) {
+          // Return a list with an empty string
+          std::cout << "Not enough options provided for: " << argv << std::endl;
+          return {""};
+        }
+      }
+    } else if (i > startingIndex && i <= startingIndex + count) {
+      // Add the string to the list
+      toReturn.push_back(mArgv[i]);
+    }
+  }
+
+  // If the arg does not exist
+  if (count == -1) {
+    std::cout << "This arg does not exist: " << argv << std::endl;
+
+    // Return an empty string
+    return {""};
+  }
+
+  // Return the list created
+  return toReturn;
 }
 
 /**
-* @brief Prints help info for ArgManager
-*/
-void cse::ArgManager::PrintHelp()
-{
-    std::cout << "--help has been triggered\n";
-    std::cout << "arguments available:\n";
-    std::cout << "-h | The help command" << std::endl;
-    std::cout << "-o | Gets the string name after it" << std::endl;
-    std::cout << "-l | The integer given after determines the list size, every string after it will be added to the list until it reaches capacity" << std::endl;
+ * @brief Prints help info for ArgManager
+ */
+void cse::ArgManager::PrintHelp() {
+  std::cout << "--help has been triggered\n";
+  std::cout << "arguments available:\n";
+  std::cout << "-h | The help command" << std::endl;
+  std::cout << "-o | Gets the string name after it" << std::endl;
+  std::cout
+      << "-l | The integer given after determines the list size, every string "
+         "after it will be added to the list until it reaches capacity"
+      << std::endl;
 }
--- Group-06/ArgManager/ArgManager.h
+++ Group-06/ArgManager/ArgManager.h
@@ -6,35 +6,34 @@
 
 #ifndef ARGMANAGER__ARGMANAGER_H
 #define ARGMANAGER__ARGMANAGER_H
-
+ 
+#include <functional>
 #include <iostream>
 #include <vector>
-#include <functional>
-
+ 
 namespace cse {
+ 
+/**
+ * @brief the ArgManager class
+ */
+class ArgManager {
+ public:
+  ArgManager(int argc, char *argv[]);
+  void LoadArgManager();
+  bool Has(std::string argv);
+  std::string GetOption(std::string argv);
+  std::vector<std::string> GetOptions(std::string argv);
+  void PrintHelp();
 
-    /**
-     * @brief the ArgManager class
-     */
-    class ArgManager {
-    public:
-        ArgManager(int argc, char *argv[]);
-        void LoadArgManager();
-        bool Has(std::string argv);
-        std::string GetOption(std::string argv);
-        std::vector<std::string> GetOptions(std::string argv);
-        void PrintHelp();
-        
-    private:
-        //store args
+ private:
+  // store args
 
-        /// @brief number of arguments
-        int mArgc = 0;
+  /// @brief number of arguments
+  int mArgc = 0;
 
-        /// @brief List of options that have been given
-        std::vector<std::string> mArgv;
-    };
-}
+  /// @brief List of options that have been given
+  std::vector<std::string> mArgv;
+};
+}  // namespace cse
 
-#endif //ARGMANAGER__ARGMANAGER_H
-
+#endif  // ARGMANAGER__ARGMANAGER_H
