52bf0146e94c0b4ea22c7ba3e3b6a8ca6ab21e51
GraceFitzgerald <fitzg256@msu.edu>
2025-02-17 19:39:22 -0500

    Updated EventManager to handle event id creation. Changed PauseEvent and ResumeEvent to take in an event instead of an id

--- Group-09/EventManager/EventManager.hpp
+++ Group-09/EventManager/EventManager.hpp
@@ -14,6 +14,7 @@
 #include <utility>
 #include <set>
 #include <cassert>
+#include <map>
 #include "../Event/Event.hpp"
 #include "../EventQueue/EventQueue.hpp"
 
@@ -22,20 +23,23 @@ namespace cse {
 class EventManager {
  private:
   EventQueue event_queue_;
-  int clock_time_ = 0; //Temporary to keep track of time
-  std::atomic<bool> running_{false};
+  int clock_time_;
+  int next_id_;
+  std::atomic<bool> running_;
   std::thread clock_thread_;
-  std::set<int> paused_events_;
   std::set<int> running_events_;
+  std::unordered_map<int, Event> paused_events_;
+  std::unordered_map<int, int> repeat_events_;
   void AdvanceTime();
   void TriggerEvents();
 
  public:
-  EventManager() = default;
+  EventManager() : clock_time_(0), next_id_(0), running_(false) {};
   ~EventManager() { StopQueue(); };
-  bool PauseEvent(int event_id);
-  bool ResumeEvent(int event_id);
-  bool AddEvent(cse::Event &event);
+  bool PauseEvent(Event &event);
+  bool ResumeEvent(Event &event_id);
+  std::optional<Event> AddEvent(int time, std::string data);
+  bool RepeatEvent(cse::Event &event, int time_interval);
   void StopQueue();
   void StartQueue();
   void RestartQueue();
