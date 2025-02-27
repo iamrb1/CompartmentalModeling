915b770765aad0ffca8dd4fcb67165c2b1bc879e
Bram Hogg <hoggbram@msu.edu>
2025-02-27 18:01:14 -0500

    updated stategridpos from reviews

--- Group-02/src/cse/StateGridPosition.cpp
+++ Group-02/src/cse/StateGridPosition.cpp
@@ -5,13 +5,19 @@
 
 #include <cse/StateGridPosition.h>
 #include <cassert>
+#include <cmath>
 
 namespace cse {
+
+const int MAX_ROTATION = 360;
+
 Point StateGridPosition::calculate_object_position() {
   ///Calculate the position of the object and return the point
   Point default_object_position;
-  default_object_position.x = 0;
-  default_object_position.y = 0;
+
+  default_object_position.x_position = 0;
+  default_object_position.y_position = 0;
+
   assert(m_state_grid == nullptr && "StateGrid is nullptr by default");
 
   return default_object_position;
@@ -19,21 +25,45 @@ Point StateGridPosition::calculate_object_position() {
 
 ///Get the orientation of the object, 0-359 degrees
 double StateGridPosition::get_object_orientation() {
-  while (m_object_orientation >= 360) {
-    m_object_orientation -= 360;
-  }
-  while (m_object_orientation < 0) {
-    m_object_orientation += 360;
-  }
+    m_object_orientation = std::fmod(m_object_orientation, MAX_ROTATION);
+    if (m_object_orientation < 0) {
+        m_object_orientation += MAX_ROTATION;
+    }
   assert(m_object_orientation >= 0 && "orientation isn't negative");
   return m_object_orientation;
 }
+
 StateGridPosition::StateGridPosition() {
-  m_central_position = create_point(0, 0);
+  Point p;
+  p.x_position = 0;
+  p.y_position = 0;
+  m_central_position = p;
   m_object_orientation = 0;
   m_state_grid = nullptr;
-  m_object_position = create_point(0, 0);
-  assert(m_object_position.x == 0 && m_object_position.y == 0 && "default object position is (0, 0)");
+
+  m_object_position = p;
+  assert(m_object_position.x_position == 0 && m_object_position.y_position == 0 && "default object position is (0, 0)");
 }
+
+void StateGridPosition::set_object_position(std::pair<int, int> agent) {
+    Point p;
+    p.x_position = static_cast<double>(agent.first);
+    p.y_position = static_cast<double>(agent.second);
+    m_object_position = p;
+}
+
+void StateGridPosition::set_object_position(double x, double y) {
+    Point p;
+    p.x_position = x;
+    p.y_position = y;
+    m_object_position = p;
+}
+
+//Point StateGridPosition::create_point(double x, double y) {
+//    Point p;
+//    p.x_position = x;
+//    p.y_position = y;
+//    return p;
+//}
 
 }  // namespace cse
