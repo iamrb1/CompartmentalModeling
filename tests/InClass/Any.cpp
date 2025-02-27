0115f930fae8569e97334bd4d9f1215300328c05
Charles Ofria <charles.ofria@gmail.com>
2025-02-27 12:23:49 -0500

    Updated tests for Any.

--- tests/InClass/Any.cpp
+++ tests/InClass/Any.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 #include "../../third-party/Catch/single_include/catch2/catch.hpp"
 
 #include "../../InClass/Group-00/Any.hpp"
@@ -5,7 +7,20 @@
 TEST_CASE("Test cse::Any", "[base]")
 {
   cse::Any any1(100);
-  cse::Any any2("abcd");
+  std::string str("abcd");
+  cse::Any any2(str);
 
   CHECK(any1.Get<int>() == 100);
+  CHECK(any2.Get<std::string>() == "abcd");
+
+  CHECK(any1.Is<int>() == true);
+  CHECK(any1.Is<bool>() == false);
+
+  any2 = 3.141592653589793238;
+  CHECK(any2.Get<double>() == 3.141592653589793238);
+
+  any2 = std::move(any1);
+  CHECK(any2.Get<int>() == 100);
+
+  CHECK(any2.AsString() == "100");
 }
