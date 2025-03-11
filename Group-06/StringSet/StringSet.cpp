eee1d697c109318a95f404e306607ff3dcdd3f3e
BuildTools <bega.ivan03@gmail.com>
2025-03-11 19:07:36 -0400

    add IsSubset and SubstringFilter

--- Group-06/StringSet/StringSet.cpp
+++ Group-06/StringSet/StringSet.cpp
@@ -99,3 +99,36 @@ void cse::StringSet::RetainFilter(const std::function<bool(const std::string&)> 
 void cse::StringSet::RemoveFilter(const std::function<bool(const std::string&)> filter) {
   Filter(filter, true);
 }
+
+/**
+ * @brief Checks if a current set is a subset of another set
+ * 
+ * @param other Other set that is a superset 
+ * @return true Current set is a subset of other set
+ * @return false Current set is not a subset of other set
+ */
+bool cse::StringSet::IsSubset(const StringSet &other) const{
+  // Prepare a lambda that checks if captured element is in the other set
+  // Used DeepSeek to assist with this lambda
+  auto otherPredicate = [&other] (const std::string& element) {
+    return other.count(element) == 1;
+  };
+
+  // use all_of in ranges to loop through each element in mElements
+  return std::ranges::all_of(mElements, otherPredicate);
+}
+
+/**
+ * @brief Returns a StringSet with words that contain a substring
+ * 
+ * @param substring Substring to check against
+ */
+void cse::StringSet::SubstringFilter(const std::string &substring) {
+  // Lambda that will be used in RemoveFilter to filter words that we need
+  auto substringPredicate = [&substring] (const std::string& element) {
+    return element.find(substring) != std::string::npos;
+  };
+
+  RemoveFilter(substringPredicate);
+
+}
