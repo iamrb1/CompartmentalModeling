aabd234509d9f0b112267acec1f4046d7ee000e4
ty <pritch76@msu.edu>
2025-03-14 12:53:51 -0400

    Fixed test cases to (almost) all pass. Still need to fix GraphPosition Test cases.

--- tests/Group-01/Graph/Edge.cpp
+++ tests/Group-01/Graph/Edge.cpp
@@ -8,12 +8,16 @@
 
 using Catch::Matchers::WithinAbs;
 
-TEST_CASE("Test cse::Edge", "[base]")
-{
-  auto v1 = std::make_shared<cse::Vertex>("id1");
-  auto v2 = std::make_shared<cse::Vertex>("id2");
+TEST_CASE("Test cse::Edge", "[base]") {
+  // Use std::string as the VERTEX_DATA_T type for testing
+  using TestVertex = cse::Vertex<std::string>;
+  using TestEdge = cse::Edge<std::string>;
 
-  auto e1 = std::make_shared<cse::Edge>("edge1", v1, v2);
+  auto v1 = std::make_shared<TestVertex>("id1", "Vertex1 Data");
+  auto v2 = std::make_shared<TestVertex>("id2", "Vertex2 Data");
+
+  auto e1 = std::make_shared<TestEdge>("edge1", v1, v2);
+
   CHECK(e1->IsConnected(*v1, *v2));
   CHECK(!e1->IsConnected(*v2, *v1));
   REQUIRE_THAT(e1->GetWeight(), WithinAbs(0, cse_test_utils::FLOAT_DELTA));
