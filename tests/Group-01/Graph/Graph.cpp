f545be8d61b2f1a10fab759b848f9022043c24a4
ty <pritch76@msu.edu>
2025-03-14 20:40:39 -0400

    Fixed test cases

--- tests/Group-01/Graph/Graph.cpp
+++ tests/Group-01/Graph/Graph.cpp
@@ -55,8 +55,8 @@ TEST_CASE("Test cse::Graph", "[base]") {
   REQUIRE_THAT(v4_v5_edge.GetWeight(), WithinAbs(0, cse_test_utils::FLOAT_DELTA));
 
   graph.RemoveEdge(e2);
-  CHECK_THROWS_AS(graph.GetEdge(v4, v5), std::runtime_error);
-  CHECK_THROWS_AS(graph.RemoveEdge(e2), std::out_of_range);
+  CHECK_THROWS_AS(graph.GetEdge(v4.GetId(), v5.GetId()), std::runtime_error);
+  CHECK_THROWS_AS(v4.GetEdge(v5), std::runtime_error);
 
   auto &e3 = graph.AddEdge("id1", "id2", 2);
   CHECK(graph.IsConnected(v1, v2));
