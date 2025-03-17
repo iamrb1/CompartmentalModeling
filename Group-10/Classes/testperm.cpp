ec8255267708f67bcb3b23819b4b191d2ae9a449
Ephraim Bennett <ephraimbennett330@gmail.com>
2025-03-17 14:19:52 -0400

    Added very basic test, not in the main testfile because there are a bunch of stuff I dont wanna mess with right now

---
+++ Group-10/Classes/testperm.cpp
@@ -0,0 +1,24 @@
+#include "PermutationManager.hpp"
+#include <iostream>
+
+
+int main() {
+  // Example usage
+  std::vector<int> items = {1, 2, 3, 4};
+  cse::PermutationManager<std::vector<int>> permManager(items, 3);
+
+  int total = permManager.permutationNumber(4, 3);
+  int t = 0;
+
+  // Print all permutations
+  do {
+      const auto& perm = permManager.getCurrentPermutation();
+      for (auto item : perm) {
+          std::cout << item << " ";
+      }
+      std::cout << std::endl;
+      t += 1;
+  } while (permManager.next());
+
+  std::cout << total << " " << t << std::endl; 
+}
