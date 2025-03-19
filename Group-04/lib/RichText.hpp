03e6e26c3b3d3b3d901c595cd0d48d429a58abbb
CircleShift <kgunger12@gmail.com>
2025-03-19 00:32:55 -0400

    [RichText] Template the RichText class

--- Group-04/lib/RichText.hpp
+++ Group-04/lib/RichText.hpp
@@ -1,6 +1,6 @@
 /**
  * @file RichText.h
- * @author Grant
+ * @author Grant, Kyle (Template stuff)
  *
  */
 
@@ -24,70 +24,78 @@
 namespace cse {
 
 /**
+ * A format in rich text.
+ * The Format struct is specialized to represent various kinds of formats in
+ * rich text. Can support simple formats like: bold, italic, strikethrough.
+ * Can support more complex formats with metadata like: font size, web links.
+ *
+ */
+struct TextFormat {
+  /**
+   * The name of the format.
+   */
+  std::string name;
+  /**
+   * The metadata associated with the format.
+   * It may be an int to represent the size of the font, a string to represent
+   * the target of a hyperlink, etc.
+   */
+  std::variant<std::string, int32_t, std::monostate> metadata;
+
+  /**
+   * Constructs a format with metadata
+   * @param name The name of the format
+   * @param metadata The metadata of the format
+   */
+  TextFormat(std::string name,
+         std::variant<std::string, int32_t, std::monostate> metadata)
+      : name(std::move(name)), metadata(std::move(metadata)) {}
+
+  /**
+   * Constructs a simple format without metadata
+   * @param name The name of the format
+   */
+  explicit(false) TextFormat(std::string name)
+      : name(std::move(name)), metadata(std::monostate()) {}
+
+  /**
+   * Compares the name first then the metadata
+   * @param r right hand side
+   * @return True if this is less than the right hand side
+   */
+  bool operator<(const TextFormat& r) const {
+    if (name == r.name) return metadata < r.metadata;
+    return name < r.name;
+  }
+
+  /**
+   * Compares the name and the metadata
+   * @param r right hand side
+   * @return True if equivalent
+   */
+  bool operator==(const TextFormat& r) const {
+    return name == r.name && metadata == r.metadata;
+  }
+};
+
+/**
  * Stores Rich Text.
  * RichText stores and manipulates formatted rich text.
  * Provides tools to represent various formats.
  * Provides tools to serialize formatted text into various formats.
  */
+template<
+  typename CharT = char,
+  class Traits = std::char_traits<CharT>,
+  class Allocator = std::allocator<CharT>
+>
 class RichText {
  public:
-  /**
-   * A format in rich text.
-   * The Format struct is specialized to represent various kinds of formats in
-   * rich text. Can support simple formats like: bold, italic, strikethrough.
-   * Can support more complex formats with metadata like: font size, web links.
-   */
-  struct Format {
-    /**
-     * The name of the format.
-     */
-    std::string name;
-    /**
-     * The metadata associated with the format.
-     * It may be an int to represent the size of the font, a string to represent
-     * the target of a hyperlink, etc.
-     */
-    std::variant<std::string, int32_t, std::monostate> metadata;
-
-    /**
-     * Constructs a format with metadata
-     * @param name The name of the format
-     * @param metadata The metadata of the format
-     */
-    Format(std::string name,
-           std::variant<std::string, int32_t, std::monostate> metadata)
-        : name(std::move(name)), metadata(std::move(metadata)) {}
-
-    /**
-     * Constructs a simple format without metadata
-     * @param name The name of the format
-     */
-    explicit(false) Format(std::string name)
-        : name(std::move(name)), metadata(std::monostate()) {}
-
-    /**
-     * Compares the name first then the metadata
-     * @param r right hand side
-     * @return True if this is less than the right hand side
-     */
-    bool operator<(const RichText::Format& r) const {
-      if (name == r.name) return metadata < r.metadata;
-      return name < r.name;
-    }
-
-    /**
-     * Compares the name and the metadata
-     * @param r right hand side
-     * @return True if equivalent
-     */
-    bool operator==(const RichText::Format& r) const {
-      return name == r.name && metadata == r.metadata;
-    }
-  };
 
  private:
-  std::string m_text;
-  std::map<Format, cse::IndexSet> m_formatting;
+  using string = std::basic_string<CharT, Traits, Allocator>;
+  string m_text;
+  std::map<TextFormat, cse::IndexSet> m_formatting;
 
  public:
   RichText() = default;
@@ -97,20 +105,34 @@ class RichText {
   RichText& operator=(RichText&&) = default;
   ~RichText() = default;
 
-  explicit RichText(std::string text) : m_text(std::move(text)) {}
-  explicit RichText(const char* text) : m_text(text) {}
+  explicit RichText(string text) : m_text(std::move(text)) {}
+  explicit RichText(const CharT* text) : m_text(text) {}
+
+  // Assign a string to RichText
+  RichText& operator=(const string& text) {
+    m_text = text;
+    m_formatting.clear();
+    return *this;
+  }
+
+  // Assign and move a string into RichText
+  RichText& operator=(string&& text) {
+    m_text = std::move(text);
+    m_formatting.clear();
+    return *this;
+  }
 
   [[nodiscard]] size_t size() const noexcept { return m_text.size(); }
 
-  [[nodiscard]] const char& char_at(size_t pos) const { return m_text.at(pos); }
+  [[nodiscard]] const CharT& char_at(size_t pos) const { return m_text.at(pos); }
 
-  [[nodiscard]] std::string to_string() const { return m_text; }
+  [[nodiscard]] string to_string() const { return m_text; }
 
-  [[nodiscard]] std::vector<Format> formats_at(size_t pos) const {
+  [[nodiscard]] std::vector<TextFormat> formats_at(size_t pos) const {
     dbg_assert(pos < m_text.size(),
                std::format("Out of bounds access, idx: {} size: {}", pos,
                            m_text.size()));
-    std::vector<Format> result;
+    std::vector<TextFormat> result;
     for (const auto& [format, index] : m_formatting) {
       if (index.contains(pos)) result.push_back(format);
     }
@@ -147,7 +169,7 @@ class RichText {
     return *this;
   }
 
-  void apply_format_to_range(const Format& format, const size_t begin,
+  void apply_format_to_range(const TextFormat& format, const size_t begin,
                              const size_t end) {
     dbg_assert(
         end >= begin,
@@ -163,14 +185,26 @@ class RichText {
   RichText& operator+=(const RichText& str) { return append(str); }
 
   [[nodiscard]] std::optional<IndexSet> get_format_range(
-      const Format& format) const {
+      const TextFormat& format) const {
     const auto iter = m_formatting.find(format);
     if (iter == m_formatting.end()) return {};
     return {iter->second};
   }
 
+  RichText& insert(size_t index, const string &str) {
+    m_text.insert(index, str);
+    // TODO: Shift over formatting
+    return *this;
+  }
+
+  RichText& insert(size_t index, const RichText<CharT> &str) {
+    m_text.insert(index, str.m_text);
+    // TODO: Shift over formatting and insert formatting from str
+    return *this;
+  }
+
   // TODO change std::invocable argument to T& once RichText is templated
-  template <std::invocable<std::string&> Callable>
+  template <std::invocable<string&> Callable>
   void update(Callable const& callable) {
     size_t const old_size = m_text.size();
     std::optional<IndexSet> const indices_opt = callable(m_text);
