f1ac964a80e1524fd3fc1ee8553561d35d426714
Kelsi <ellio357@msu.edu>
2025-03-26 16:13:13 -0400

    updated TagManager Testing

--- Group-08/Dev/Tests/TagManagerTest/TagManagerTest.cpp
+++ Group-08/Dev/Tests/TagManagerTest/TagManagerTest.cpp
@@ -18,10 +18,10 @@ TEST_CASE("Test addTag", "[TagManager]") {
     tm.addTag("personal", "medium");
 
 
-    assert(tm.getTags("school").size() == 3);
-    assert(tm.getTags("work").size() == 1);
-    assert(tm.getTags("personal").size() == 1);
-    assert(tm.getTags("CSE498").empty());
+    CHECK(tm.getTags("school").size() == 3);
+    CHECK(tm.getTags("work").size() == 1);
+    CHECK(tm.getTags("personal").size() == 1);
+    CHECK(tm.getTags("CSE498").empty());
 
 }
 
@@ -33,10 +33,10 @@ TEST_CASE("Test removeTag", "[TagManager]") {
 
     tm.removeTag("CSE498", "work");
 
-    assert(tm.getTags("CSE498").size() == 1);
+    CHECK(tm.getTags("CSE498").size() == 1);
 
     tm.removeTag("CSE498", "no");
-    assert(tm.getTags("CSE498").size() == 1);
+    CHECK(tm.getTags("CSE498").size() == 1);
 }
 
 TEST_CASE("Test getTag", "[TagManager]") {
@@ -46,12 +46,12 @@ TEST_CASE("Test getTag", "[TagManager]") {
     tm.addTag("CSE498", "work");
 
     auto tags = tm.getTags("CSE498");
-    assert(tags.size() == 2);
-    assert(tags.count("urgent") == 1);
-    assert(tags.count("work") == 1);
+    CHECK(tags.size() == 2);
+    CHECK(tags.count("urgent") == 1);
+    CHECK(tags.count("work") == 1);
 
     tags = tm.getTags("no");
-    assert(tags.empty());
+    CHECK(tags.empty());
 }
 
 TEST_CASE("Test getTaskTags", "[TagManager]") {
@@ -62,9 +62,9 @@ TEST_CASE("Test getTaskTags", "[TagManager]") {
     tm.addTag("school", "urgent");
 
     auto tasks = tm.getTaskTags("urgent");
-    assert(tasks.size() == 2);
-    assert(tasks.count("CSE498") == 1);
-    assert(tasks.count("school") == 1);
+    CHECK(tasks.size() == 2);
+    CHECK(tasks.count("CSE498") == 1);
+    CHECK(tasks.count("school") == 1);
 }
 
 TEST_CASE("Test clearTagsForTask", "[TagManager]") {
@@ -74,10 +74,16 @@ TEST_CASE("Test clearTagsForTask", "[TagManager]") {
     tm.addTag("CSE498", "work");
     tm.addTag("school", "urgent");
 
+    CHECK(tm.getTags("CSE498").size()== 2);
+    auto tasks = tm.getTags("CSE498");
+    CHECK(tasks.count("urgent") == 1);
+    CHECK(tasks.count("work") == 1);
+    CHECK(tm.getTags("school").size() == 1);
+
     tm.clearTagsForTask("CSE498");
 
-    assert(tm.getTags("CSE498").empty());
-    assert(tm.getTags("school").size() == 1);
+    CHECK(tm.getTags("CSE498").empty());
+    CHECK(tm.getTags("school").size() == 1);
 }
 
 TEST_CASE("Test clearTags", "[TagManager]") {
@@ -89,8 +95,8 @@ TEST_CASE("Test clearTags", "[TagManager]") {
 
     tm.clearTags("urgent");
 
-    assert(tm.getTags("school").empty());
-    assert(tm.getTags("CSE498").size() == 1);
+    CHECK(tm.getTags("school").empty());
+    CHECK(tm.getTags("CSE498").size() == 1);
 }
 
 TEST_CASE("Test hasTag", "[TagManager]") {
@@ -98,7 +104,7 @@ TEST_CASE("Test hasTag", "[TagManager]") {
 
     tm.addTag("CSE498", "urgent");
 
-    assert(tm.hasTag("CSE498", "urgent"));
+    CHECK(tm.hasTag("CSE498", "urgent"));
     CHECK_FALSE(tm.hasTag("CSE498", "no"));
     CHECK_FALSE(tm.hasTag("school", "urgent"));
 }
@@ -112,7 +118,7 @@ TEST_CASE("Test removing a tag removes it from task", "[TagManager]") {
     tm.removeTag("CSE498", "urgent");
 
     CHECK_FALSE(tm.hasTag("CSE498", "urgent"));
-    assert(tm.hasTag("CSE498", "work"));
+    CHECK(tm.hasTag("CSE498", "work"));
 }
 
 TEST_CASE("Test clearTagsForTask remove all associations", "[TagManager]") {
@@ -136,7 +142,7 @@ TEST_CASE("Test clearTags remove all associations", "[TagManager]") {
 
     tm.clearTags("urgent");
 
-    assert(tm.hasTag("CSE498", "work"));
+    CHECK(tm.hasTag("CSE498", "work"));
     CHECK_FALSE(tm.hasTag("CSE498", "urgent"));
     CHECK_FALSE(tm.hasTag("school", "urgent"));
 }
