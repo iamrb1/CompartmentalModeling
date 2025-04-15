e6c573749b095db51f2d00642ef5d20e6536ef2c
Max Krawec <krawecma@msu.edu>
2025-04-15 18:11:33 -0400

    Worked more on the main application
    1) Added helper functions
    1.1) Checks if the user input is a double
    1.2) Checks if the user input is an int
    1.3) Gets the column index from the user
    1.4) Gets a Datum value from the user
    
    2) Improved the math menu
    3) Created the comparison menu

--- Group-05/main.cpp
+++ Group-05/main.cpp
@@ -10,15 +10,103 @@
 #include <iostream>
 
 /**
+ * Helper function that determines if the user input is a double
+ * @param string_value The user input
+ * @return Double optional if the input is a double, otherwise an empty optional
+ */
+std::optional<double> IsValidDouble(const std::string &string_value) {
+  // CITE: Used ChatGPT to write this code. Fixed a bug where strings starting with
+  // numbers (e.g., "123Hello") were partially converted instead of being a string.
+  // Also used in the IsValidInt() function
+  try {
+    std::size_t pos;
+    double double_value = std::stod(string_value, &pos);
+    if (pos == string_value.length()) {
+      return double_value;
+    }
+  } catch(std::invalid_argument &e) {}
+  return {};
+}
+
+
+/**
+ * Helper function that determines if the user input is an int
+ * @param string_value The user input
+ * @return Int optional if the input is a int, otherwise an empty optional
+ */
+std::optional<int> IsValidInt(const std::string &string_value) {
+  try {
+    std::size_t pos;
+    int int_value = std::stoi(string_value, &pos);
+    if (pos == string_value.length()) {
+      return int_value;
+    }
+  } catch(std::invalid_argument &e) {}
+  return {};
+}
+
+
+/**
+ * Helper function that gets the column index from the user
+ * @return The column index the user specified
+ */
+int GetColumnIndex() {
+  while (true) {
+    std::cout << "Please enter column index: ";
+    std::string index_string;
+    std::cin >> index_string;
+
+    std::optional<int> int_value = IsValidInt(index_string);
+    if (int_value.has_value()) {
+      return int_value.value();
+    }
+    else {
+      std::cout << "Invalid option. Try again." << std::endl;
+    }
+  }
+}
+
+/**
+ * Helper function that gets the data (Datum) value from the user
+ * @return The data (Datum) value the user specified
+ */
+cse::Datum GetDataValue() {
+  std::cout << "Please enter the value to compare: ";
+  std::string compare_value;
+  std::cin >> compare_value;
+
+  std::optional<double> double_value = IsValidDouble(compare_value);
+  if (double_value.has_value()) {
+    return {double_value.value()};
+  }
+
+  return {compare_value};
+}
+
+/**
+ * Helper function that prints a column
+ * @param column_reference_vector The column
+ */
+void PrintColumn(const cse::ReferenceVector<cse::Datum> &column) {
+  for (const cse::Datum &value : column) {
+    if (value.IsDouble()) {
+      std::cout << value.GetDouble() << " ";
+    } else {
+      std::cout << value.GetString() << " ";
+    }
+  }
+  std::cout << "" << std::endl;
+}
+
+/**
  * Outputs options for the grid menu
  */
-
 cse::DataGrid CreateGrid() {
   std::size_t number_of_columns = 0;
   std::size_t number_of_rows = 0;
   std::string default_numbers;
 
-    // Prompt for number of columns
+  // Prompt for number of columns
   while (true) {
     std::cout << "Enter number of rows for your DataGrid: ";
     std::cin >> number_of_rows;
@@ -83,8 +171,7 @@ cse::DataGrid CreateGrid() {
 }
 
 cse::DataGrid GridMenu() {
-  bool show_csv_menu = true;
-  while (show_csv_menu) {
+  while (true) {
     std::cout << "Please enter an option from the list below" << std::endl;
 
     std::cout << "i: Import a CSV file (TODO)" << std::endl;
@@ -97,11 +184,13 @@ cse::DataGrid GridMenu() {
 
     if (option == "i") {
       // TODO
-      show_csv_menu = false;
+      break;
     } else if (option == "e") {
       // TODO
-      show_csv_menu = false;
-    } else if (option == "t") {
+      break;
+    }
+    // TODO - remove
+    else if (option == "t") {
       std::vector<std::vector<cse::Datum>> math_vector(5, std::vector<cse::Datum>(3));
 
       math_vector[0][0] = cse::Datum(5.0);
@@ -114,12 +203,12 @@ cse::DataGrid GridMenu() {
       math_vector[1][1] = cse::Datum("test2");
       math_vector[2][1] = cse::Datum("test4");
       math_vector[3][1] = cse::Datum("test5");
+      math_vector[4][1] = cse::Datum("test6");
 
       math_vector[0][2] = cse::Datum(10.25);
       math_vector[1][2] = cse::Datum("test3");
       math_vector[2][2] = cse::Datum(150.50);
       math_vector[3][2] = cse::Datum(200);
-      math_vector[4][1] = cse::Datum("test6");
       math_vector[4][2] = cse::Datum(20.25);
 
       cse::DataGrid grid(math_vector);
@@ -130,123 +219,151 @@ cse::DataGrid GridMenu() {
       std::cout << "Invalid option. Try again." << std::endl;
     }
   }
-  return cse::DataGrid(); // TODO - This should never be called. Maybe do something else here?
+
+  // CITE: Used ChatGPT for this idea.
+  // TODO - Should be able to remove after people add their code
+  throw std::runtime_error("Unexpected exit from menu loop");
 }
 
 /**
- * Outputs options for the math menu
+ * Outputs the options and results for the math menu
  */
 void MathMenu(const cse::DataGrid& grid) {
-  std::cout << "Please enter a math option from the list below:" << std::endl;
-  std::cout << "cmean: Calculate the mean of a column" << std::endl;
-  std::cout << "cmed: Calculate the median of a column" << std::endl;
-  std::cout << "cmode: Calculate the mode of a column" << std::endl;
-  std::cout << "csd: Calculate the standard deviation of a column" << std::endl;
-  std::cout << "cmin: Calculate the min of a column" << std::endl;
-  std::cout << "cmax: Calculate the max of a column" << std::endl;
-  std::cout << "sum: Calculate the mean, median, mode, standard deviation, min, and max of the data grid" << std::endl;
-  std::cout << "b: Go back" << std::endl;
+  while(true) {
+    std::cout << "Please enter a math option from the list below:" << std::endl;
+    std::cout << "cmean: Calculate the mean of a column" << std::endl;
+    std::cout << "cmed: Calculate the median of a column" << std::endl;
+    std::cout << "cmode: Calculate the mode of a column" << std::endl;
+    std::cout << "csd: Calculate the standard deviation of a column" << std::endl;
+    std::cout << "cmin: Calculate the min of a column" << std::endl;
+    std::cout << "cmax: Calculate the max of a column" << std::endl;
+    std::cout << "sum: Calculate the mean, median, mode, standard deviation, min, and max of the data grid" << std::endl;
+    std::cout << "b: Go back" << std::endl;
 
-  std::string option;
-  std::cin >> option;
+    std::string option;
+    std::cin >> option;
 
-  // Used ChatGPT to help write a lambda to simplify the code
-  auto GetColumnIndex = []() {
-    std::cout << "Please enter column index: ";
-    int index;
-    std::cin >> index;
-    return index;
-  };
-
-  if (option == "cmean") {
-    int value = GetColumnIndex();
-    std::cout << "The mean at column " << value << " is: " << grid.ColumnMean(value) << std::endl;
-  } else if (option == "cmed") {
-    int value = GetColumnIndex();
-    std::cout << "The median at column " << value << " is: " << grid.ColumnMedian(value) << std::endl;
-  } else if (option == "csd") {
-    int value = GetColumnIndex();
-    std::cout << "The standard deviation at column " << value << " is: " << grid.ColumnStandardDeviation(value) << std::endl;
-  } else if (option == "cmin") {
-    int value = GetColumnIndex();
-    std::cout << "The min at column " << value << " is: " << grid.ColumnMin(value) << std::endl;
-  } else if (option == "cmax") {
-    int value = GetColumnIndex();
-    std::cout << "The max at column " << value << " is: " << grid.ColumnMax(value) << std::endl;
-  } else if (option == "cmode") {
-    int value = GetColumnIndex();
-    std::cout << "The mode(s) at column " << value << " is/are: ";
-    for (const double& mode_value : grid.ColumnMode(value)) {
-      std::cout << mode_value << ' ';
-    }
-    std::cout << "" << std::endl;
-  } else if (option == "sum") {
-    auto summary = grid.CalculateDataGridMathSummary();
-    std::cout << "The summary of the grid is:" << std::endl;
-    std::cout << "Mean " << summary.mean << std::endl;
-    std::cout << "Median " << summary.median << std::endl;
-    std::cout << "Standard Deviation " << summary.standardDeviation << std::endl;
-    std::cout << "Min " << summary.min << std::endl;
-    std::cout << "Max " << summary.max << std::endl;
-
-    std::cout << "Mode(s) ";
-    for (const double& mode_value : summary.mode) {
-      std::cout << mode_value << ' ';
-    }
-    std::cout << "" << std::endl;
-  } else if (option != "b") {
-    std::cout << "Invalid option. Try again." << std::endl;
-  }
-  std::cout << "" << std::endl;
-}
-
-// TODO - WIP (const) - Max K.
-void ComparisonMenu(cse::DataGrid& grid) {
-  std::cout << "Please enter a comparison option from the list below:" << std::endl;
-  std::cout << "clt: Find values less than a given value for a column" << std::endl;
-  std::cout << "clte: Find values less than or equal to a given value for a column" << std::endl;
-  std::cout << "cgt: Find values greater than a given value for a column" << std::endl;
-  std::cout << "cgte: Find values greater than or equal to a given value for a column" << std::endl;
-  std::cout << "ce: Find values equal to a value for a column" << std::endl;
-  std::cout << "cne: Find values not equal to a given value for a column" << std::endl;
-  std::cout << "b: Go back" << std::endl;
-
-  std::string option;
-  std::cin >> option;
-
-  auto GetCompareValue = []() {
-    std::cout << "Do you want to compare a number (double) or text (string): " << std::endl;
-    std::cout << "n: double (number) " << std::endl;
-    std::cout << "s: string (text) " << std::endl;
-    std::string datum_type_value;
-    std::cin >> datum_type_value;
-
-    while (true) {
-      std::string value;
-      if (datum_type_value == "n") {
-        std::cout << "Please enter the value to compare: ";
-        std::cin >> value;
-        return cse::Datum(value);
-      } else if (datum_type_value == "t") {
-        std::cout << "Please enter the value to compare: ";
-        std::cin >> value;
-        return cse::Datum(value);
-      } else {
-        std::cout << "Invalid option. Try again." << std::endl;
+    if (option == "cmean") {
+      int index = GetColumnIndex();
+      std::cout << "The mean at column " << index << " is: " << grid.ColumnMean(index) << std::endl;
+      return;
+    } else if (option == "cmed") {
+      int index = GetColumnIndex();
+      std::cout << "The median at column " << index << " is: " << grid.ColumnMedian(index) << std::endl;
+      return;
+    } else if (option == "csd") {
+      int index = GetColumnIndex();
+      std::cout << "The standard deviation at column " << index << " is: " << grid.ColumnStandardDeviation(index) << std::endl;
+      return;
+    } else if (option == "cmin") {
+      int index = GetColumnIndex();
+      std::cout << "The min at column " << index << " is: " << grid.ColumnMin(index) << std::endl;
+      return;
+    } else if (option == "cmax") {
+      int index = GetColumnIndex();
+      std::cout << "The max at column " << index << " is: " << grid.ColumnMax(index) << std::endl;
+      return;
+    } else if (option == "cmode") {
+      int index = GetColumnIndex();
+      std::cout << "The mode(s) at column " << index << " is/are: ";
+      for (const double& mode_value : grid.ColumnMode(index)) {
+        std::cout << mode_value << ' ';
       }
-    }
-  };
+      std::cout << "" << std::endl;
+      return;
+    } else if (option == "sum") {
+      auto summary = grid.CalculateDataGridMathSummary();
+      std::cout << "The summary of the grid is:" << std::endl;
+      std::cout << "Mean " << summary.mean << std::endl;
+      std::cout << "Median " << summary.median << std::endl;
+      std::cout << "Standard Deviation " << summary.standardDeviation << std::endl;
+      std::cout << "Min " << summary.min << std::endl;
+      std::cout << "Max " << summary.max << std::endl;
 
-  auto GetColumnIndex = []() {
-    std::cout << "Please enter column index: ";
-    int index;
-    std::cin >> index;
-    return index;
-  };
+      std::cout << "Mode(s) ";
+      for (const double& mode_value : summary.mode) {
+        std::cout << mode_value << ' ';
+      }
+      std::cout << "" << std::endl;
+      return;
+    } else if (option == "b") {
+      return;
+    } else {
+      std::cout << "Invalid option. Try again." << std::endl;
+    }
+  }
 }
+
+/**
+ * Outputs the options and results for the comparison menu
+ * @param grid The DataGrid
+ */
+void ComparisonMenu(cse::DataGrid& grid) {
+  while(true) {
+    std::cout << "Please enter a comparison option from the list below:" << std::endl;
+    std::cout << "clt: Find values less than a given value for a column" << std::endl;
+    std::cout << "clte: Find values less than or equal to a given value for a column" << std::endl;
+    std::cout << "cgt: Find values greater than a given value for a column" << std::endl;
+    std::cout << "cgte: Find values greater than or equal to a given value for a column" << std::endl;
+    std::cout << "ce: Find values equal to a value for a column" << std::endl;
+    std::cout << "cne: Find values not equal to a given value for a column" << std::endl;
+    std::cout << "b: Go back" << std::endl;
+
+    std::string option;
+    std::cin >> option;
+
+    if (option=="clt") {
+      int index = GetColumnIndex();
+      cse::Datum datum = GetDataValue();
+      std::cout << "The less than values are: " << std::endl;
+      PrintColumn(grid.ColumnLessThan(index, datum));
+      return;
+    } else if (option=="clte") {
+
+      int index = GetColumnIndex();
+      cse::Datum datum = GetDataValue();
+      std::cout << "The less than or equal values are: " << std::endl;
+      PrintColumn(grid.ColumnLessThanOrEqual(index, datum));
+      return;
+    } else if (option=="cgt") {
+
+      int index = GetColumnIndex();
+      cse::Datum datum = GetDataValue();
+      std::cout << "The greater than values are: " << std::endl;
+      PrintColumn(grid.ColumnGreaterThan(index, datum));
+      return;
+    } else if (option=="cgte") {
+
+      int index = GetColumnIndex();
+      cse::Datum datum = GetDataValue();
+      std::cout << "The greater than or equal values are: " << std::endl;
+      PrintColumn(grid.ColumnGreaterThanOrEqual(index, datum));
+      return;
+    } else if (option=="ce") {
+
+      int index = GetColumnIndex();
+      cse::Datum datum = GetDataValue();
+      std::cout << "The values that are equal are: " << std::endl;
+      PrintColumn(grid.ColumnEqual(index, datum));
+      return;
+    } else if (option=="cne") {
+
+      int index = GetColumnIndex();
+      cse::Datum datum = GetDataValue();
+      std::cout << "The values that are not equal are: " << std::endl;
+      PrintColumn(grid.ColumnNotEqual(index, datum));
+      return;
+    } else if (option == "b") {
+      return;
+    } else {
+      std::cout << "Invalid option. Try again." << std::endl;
+    }
+  }
+}
+
 
 int main() {
-  std::cout << "Welcome to CSV Command Line Manipulator" << std::endl; // TODO - Determine name
+  std::cout << "Welcome to CSV Command Line Manipulator" << std::endl;
   std::cout << "Developed by: Max Krawec, Calen Green, Pedro Mitkiewicz, Shahaab Ali, and Muhammad Asif Masood" << std::endl;
   std::cout << "" << std::endl;
 
@@ -259,7 +376,7 @@ int main() {
     std::cout << "sort (TODO)" << std::endl;
     std::cout << "extra (TODO)" << std::endl;
     std::cout << "math" << std::endl;
-    std::cout << "comparisons (WIP)" << std::endl;
+    std::cout << "comparisons" << std::endl;
     std::cout << "print (TODO)" << std::endl;
     std::cout << "quit" << std::endl;
 
