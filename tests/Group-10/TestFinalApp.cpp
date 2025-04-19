7f5d0375b30c259f64c396f4360dc6b43882cafe
muell274 <muell274@msu.edu>
2025-04-19 14:12:58 -0400

    added test case and files for testing weighless flag.

--- tests/Group-10/TestFinalApp.cpp
+++ tests/Group-10/TestFinalApp.cpp
@@ -4,6 +4,7 @@
 
 #include "../../third-party/Catch/single_include/catch2/catch.hpp"
 #include "../../Group-10/Application/application.cpp"
+#include <set>
 
 
 /**
@@ -25,3 +26,70 @@ TEST_CASE("Baisc", "[FinalApp]") {
     std::cout << "FILE NOT FOUND" << std::endl;
   }
 }
+
+TEST_CASE("Weightless mode computes correct total value from script2", "[FinalApp][Weightless]") {
+  std::ifstream inputFile("script2.txt");
+  std::ostringstream capturedOutput;
+
+  // Redirect std::cout
+  auto* originalBuf = std::cout.rdbuf();
+  std::cout.rdbuf(capturedOutput.rdbuf());
+
+  if (inputFile.is_open()) {
+    application(inputFile);
+  } else {
+    std::cout << "FILE NOT FOUND" << std::endl;
+  }
+
+  std::cout.rdbuf(originalBuf); 
+
+  std::string output = capturedOutput.str();
+  std::istringstream stream(output);
+  std::string line;
+
+  double totalValueFromApp = -1.0;
+
+  while (std::getline(stream, line)) {
+    if (line.find("Optimal Value:") != std::string::npos) {
+      totalValueFromApp = std::stod(line.substr(line.find(":") + 1));
+    }
+  }
+  double expectedValue = 27.1;
+
+  REQUIRE(totalValueFromApp == Approx(expectedValue).margin(0.01));
+}
+
+//Taking way too long
+/*TEST_CASE("Repeats mode computes correct total value from script3", "[FinalApp][Repeats]") {
+  std::ifstream inputFile("script3.txt");
+  std::ostringstream capturedOutput;
+
+  // Redirect std::cout to capture output
+  auto* originalBuf = std::cout.rdbuf();
+  std::cout.rdbuf(capturedOutput.rdbuf());
+
+  if (inputFile.is_open()) {
+    application(inputFile);
+  } else {
+    std::cout << "FILE NOT FOUND" << std::endl;
+  }
+
+  std::cout.rdbuf(originalBuf); // Restore std::cout
+
+  std::string output = capturedOutput.str();
+  std::istringstream stream(output);
+  std::string line;
+
+  double totalValueFromApp = -1.0;
+
+  while (std::getline(stream, line)) {
+    if (line.find("Optimal Value:") != std::string::npos) {
+      totalValueFromApp = std::stod(line.substr(line.find(":") + 1));
+    }
+  }
+
+  double expectedValue = 20.6; //test for for sure
+
+  REQUIRE(totalValueFromApp == Approx(expectedValue).margin(0.01));
+}*/
+
