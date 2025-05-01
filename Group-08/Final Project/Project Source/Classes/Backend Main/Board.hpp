fc328c92f0aa381d3469e0d253c3e6c5c4406645
Joanna Rodriguez Zamarron <rodri896@msu.edu>
2025-05-01 19:47:45 -0400

    More Doxygen comments

--- Group-08/Final Project/Project Source/Classes/Backend Main/Board.hpp
+++ Group-08/Final Project/Project Source/Classes/Backend Main/Board.hpp
@@ -9,29 +9,66 @@
 
 namespace cse {
 
+    /**
+     * @class   Board
+     * @file    Board.hpp
+     * @author  Joanna Rodriguez Zamarron
+     * @brief   Represents a board that manages a set of unique Card objects with random access.
+     * 
+     * This class acts as the container for all cards on the task board. It allows
+     * adding, removing, and accessing cards using index-based retrieval while ensuring
+     * that each card is unique. Internally, it leverages RandomAccessSet to manage
+     * both uniqueness and fast access.
+     */
 class Board {
     private:
+        /**
+         * @brief A random-access set of unique cards stored in the board.
+         */
         cse::RandomAccessSet<Card> cards;
     
     public:
+        /**
+         * @brief Default constructor.
+         */
         Board() = default;
     
-        // When we click +
+        /**
+         * @brief Adds a new card to the board.
+         * 
+         * @param card The Card to be added.
+         * @return True if the card was successfully added; false if it already exists.
+         */
         bool addCard(const Card& card) {
             return cards.add(card);
         }
     
-        // When we click -
+        /**
+         * @brief Removes an existing card from the board.
+         * 
+         * @param card The Card to be removed.
+         * @return True if the card was successfully removed; false if it was not found.
+         */
         bool removeCard(const Card& card) {
             return cards.remove(card);
         }
     
-        // Grab a card based off index, we would have to grab one if we are doing a search ?
+        /**
+         * @brief Retrieves a card by its index in the board.
+         * 
+         * @param index The index of the card to retrieve.
+         * @return The Card at the specified index.
+         * @throws std::out_of_range if index is invalid.
+         */
         Card getCard(size_t index) const {
             return cards.get(index);
         }
         
-        // not sure what size could help with
+        /**
+         * @brief Returns the total number of cards on the board.
+         * 
+         * @return The size of the board.
+         */
         size_t size() const {
             return cards.size();
         }
--- Group-08/Final Project/Project Source/Classes/Backend Main/CardHash.hpp
+++ Group-08/Final Project/Project Source/Classes/Backend Main/CardHash.hpp
@@ -2,9 +2,32 @@
 #include "Card.hpp"
 #include <functional>
 
+/**
+ * @file    CardHash.hpp
+ * @brief   Hash function specialization for Card to allow use in unordered containers.
+ * 
+ * This file provides a specialization of std::hash for the cse::Card class,
+ * enabling Card objects to be used as keys in hash-based containers like
+ * std::unordered_map and std::unordered_set. The hash is based solely on the
+ * card's unique ID, making it consistent with the Card's equality operator.
+ */
+
 namespace std {
+    /**
+     * @struct hash
+     * @brief Specialization of std::hash for cse::Card.
+     *
+     * Generates a hash value based on the Card’s ID. This enables
+     * usage of Card objects in standard hash containers.
+     */
     template <>
     struct hash<cse::Card> {
+        /**
+         * @brief Computes a hash value for a given Card.
+         * 
+         * @param card The Card object to hash.
+         * @return std::size_t The hash value based on the card’s ID.
+         */
         std::size_t operator()(const cse::Card& card) const noexcept {
             return std::hash<int>()(card.getId());
         }
